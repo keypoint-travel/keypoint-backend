bc626f5d5fd37b6f4d2fed495120cbd46a2228f4
chea-young <lchy0413@gmail.com>
2024-05-18 22:33:12 +0900

    [Init] add springboot project
    [Issue] #2

---
+++ src/main/java/com/keypoint/keypointtravel/KeypointTravelApplication.java
@@ -0,0 +1,13 @@
+package com.keypoint.keypointtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeypointTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeypointTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keypoint/keypointtravel/KeypointTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keypoint.keypointtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeypointTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
