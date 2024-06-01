08983e4b046bd1c8076b9138635b6fcc3a1232fc
chea-young <lchy0413@gmail.com>
2024-06-01 18:59:59 +0900

    [Feature] Add 001_ACCESS_DENIED about service key
    [Issue no] #5

--- src/main/java/com/keypoint/keypointtravel/common/interceptor/ServiceKeyInterceptor.java
+++ src/main/java/com/keypoint/keypointtravel/common/interceptor/ServiceKeyInterceptor.java
@@ -1,9 +1,12 @@
 package com.keypoint.keypointtravel.common.interceptor;
 
 import com.keypoint.keypointtravel.common.constants.HeaderConstants;
+import com.keypoint.keypointtravel.common.enumType.error.AuthorityErrorCode;
+import com.keypoint.keypointtravel.common.exception.GeneralException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -27,8 +30,7 @@ public class ServiceKeyInterceptor implements HandlerInterceptor {
     if (serviceKey.equals(headerServiceKey)) {
       return true;
     } else {
-      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-      return false;
+      throw new GeneralException(HttpStatus.UNAUTHORIZED, AuthorityErrorCode.ACCESS_DENIED);
     }
   }
 }
