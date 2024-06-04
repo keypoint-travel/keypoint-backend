6d51df01aaebdf2ff538b53f971b90d5ef2b8481
chea-young <lchy0413@gmail.com>
2024-06-04 21:38:28 +0900

    [Feature] Add Member entity
    [Issue] #4

---
+++ src/main/java/com/keypoint/keypointtravel/repository/member/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.keypoint.keypointtravel.repository.member;
+
+import com.keypoint.keypointtravel.entity.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
