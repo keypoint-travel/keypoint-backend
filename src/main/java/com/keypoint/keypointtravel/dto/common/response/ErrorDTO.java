19c22af302d19dad2fd1ece8c3cb63567a648e25
chea-young <lchy0413@gmail.com>
2024-06-06 18:54:17 +0900

    [Feature] Add ResponseEntity
    [Issue] #2

--- src/main/java/com/keypoint/keypointtravel/dto/common/response/ErrorDTO.java
+++ src/main/java/com/keypoint/keypointtravel/dto/common/response/ErrorDTO.java
@@ -5,6 +5,7 @@ import com.keypoint.keypointtravel.common.exception.GeneralException;
 import com.keypoint.keypointtravel.common.exception.HttpClientException;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @Data
@@ -15,27 +16,44 @@ public class ErrorDTO {
     private String msg;
     private Object detail;
 
-    public static ResponseEntity<Object> toResponseEntity(GeneralException ex) {
-        Object detail = ex.getDetail();
-        return ResponseEntity
-            .status(ex.getStatus())
-            .body(ErrorDTO.builder()
-                .code(ex.getErrorCode())
-                .msg(ex.getErrorMsg())
-                .detail(detail)
-                .build());
+    public static APIResponseEntity<ErrorDTO> toAPIResponseEntity(GeneralException ex) {
+        ErrorDTO errorDTO = ErrorDTO.builder()
+            .code(ex.getErrorCode())
+            .msg(ex.getErrorMsg())
+            .detail(ex.getDetail())
+            .build();
+
+        return APIResponseEntity.<ErrorDTO>builder()
+            .result(false)
+            .isAuthError(ex.getStatus() == HttpStatus.UNAUTHORIZED)
+            .code(ex.getStatus())
+            .data(errorDTO)
+            .build();
     }
 
-    public static ResponseEntity<Object> toResponseEntity(
+    public static ResponseEntity<APIResponseEntity<ErrorDTO>> toResponseEntity(
+        GeneralException ex
+    ) {
+        return ResponseEntity
+            .status(ex.getStatus())
+            .body(ErrorDTO.toAPIResponseEntity(ex));
+    }
+
+    public static ResponseEntity<APIResponseEntity<ErrorDTO>> toResponseEntity(
         HttpClientException ex,
         ErrorCode code
     ) {
+        ErrorDTO errorDTO = ErrorDTO.builder()
+            .code(code.getCode())
+            .msg(ex.getMessage())
+            .build();
+
         return ResponseEntity
             .status(ex.getStatusCode())
-            .body(ErrorDTO.builder()
-                .code(code.getCode())
-                .msg(ex.getMessage())
+            .body(APIResponseEntity.<ErrorDTO>builder()
+                .result(false)
+                .code(HttpStatus.BAD_REQUEST)
+                .data(errorDTO)
                 .build());
     }
-
 }
