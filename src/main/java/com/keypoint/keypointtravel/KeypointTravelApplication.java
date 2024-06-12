e7b1e9f0301ab6ce2d70669a4b03efb8588524a4
chea-young <lchy0413@gmail.com>
2024-06-12 19:24:44 +0900

    [Fix] Add springboot setting 배포된 서비스가 멈추는 현상 수정
    [Issue] #7

--- src/main/java/com/keypoint/keypointtravel/KeypointTravelApplication.java
+++ src/main/java/com/keypoint/keypointtravel/KeypointTravelApplication.java
@@ -2,12 +2,14 @@ package com.keypoint.keypointtravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class KeypointTravelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KeypointTravelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KeypointTravelApplication.class, args);
+    }
 
 }
