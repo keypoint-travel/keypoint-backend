9fd80426052d4683c8388e4e191a2c2349b17640
chea-young <lchy0413@gmail.com>
2024-06-15 22:22:01 +0900

    [Refactor] Change to use Optional (#17)

--- src/main/java/com/keypoint/keypointtravel/member/repository/MemberRepository.java
+++ src/main/java/com/keypoint/keypointtravel/member/repository/MemberRepository.java
@@ -1,12 +1,12 @@
 package com.keypoint.keypointtravel.member.repository;
 
 import com.keypoint.keypointtravel.member.entity.Member;
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
