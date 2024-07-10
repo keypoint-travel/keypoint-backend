c07a1ebc0ade5189d915da3757507c6424a0b795
Khdscor <khds_cor@naver.com>
2024-07-10 18:44:39 +0900

    fix: 데이터가 없을 시 빈 문자열로 입력되는 문제 해결 : com.fasterxml.jackson.databind.exc.InvalidFormatException: Cannot coerce empty String ("")

--- src/main/java/com/keypoint/keypointtravel/banner/dto/useCase/tourListUseCase/Items.java
+++ src/main/java/com/keypoint/keypointtravel/banner/dto/useCase/tourListUseCase/Items.java
@@ -1,13 +1,20 @@
 package com.keypoint.keypointtravel.banner.dto.useCase.tourListUseCase;
 
+import java.util.Collections;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Items {
     private List<Item> item;
+
+    public Items(List<Item> item) {
+        this.item = item;
+    }
+
+    public Items(String item) {
+        this.item = Collections.emptyList();
+    }
 }
