93cc0efefa8052975b578f0be345e742e629a9da
Khdscor <khds_cor@naver.com>
2024-07-10 18:43:43 +0900

    refactor: Response 파일 가독성 향상

--- src/main/java/com/keypoint/keypointtravel/banner/dto/response/BannerListResponse.java
+++ src/main/java/com/keypoint/keypointtravel/banner/dto/response/BannerListResponse.java
@@ -1,6 +1,7 @@
 package com.keypoint.keypointtravel.banner.dto.response;
 
 import com.keypoint.keypointtravel.banner.dto.useCase.tourListUseCase.Body;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,9 +27,10 @@ public class BannerListResponse {
 
     public static BannerListResponse from(Body data) {
         return BannerListResponse.builder()
-            .contents(data.getItems().getItem().stream().map(BannerDetails::from).toList())
-            .last(data.getPageNo() == data.getTotalCount() / 10 + 1)
-            .totalPages(data.getTotalCount() / 10 + 1)
+            .contents(data.getItems().getItem().isEmpty()? new ArrayList<>()
+                :data.getItems().getItem().stream().map(BannerDetails::from).toList())
+            .last(data.getPageNo() == getTotalPage(data.getTotalCount()))
+            .totalPages(getTotalPage(data.getTotalCount()))
             .totalElements(data.getTotalCount())
             .first(data.getPageNo() == 1)
             .size(10)
@@ -36,6 +38,9 @@ public class BannerListResponse {
             .empty(data.getNumOfRows() == 0)
             .pageNumber(data.getPageNo())
             .build();
+    }
 
+    private static int getTotalPage(int totalCount){
+        return totalCount / 10 + ((totalCount % 10 > 0) ?1 : 0);
     }
 }
