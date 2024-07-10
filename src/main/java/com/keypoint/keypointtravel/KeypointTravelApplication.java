e15e580319b23d980d2cf106ef45f08a18ed766e
Khdscor <khds_cor@naver.com>
2024-07-10 11:24:56 +0900

    build: 한국관광공사 api 호출을 위한 openfeign 의존성 추가

--- src/main/java/com/keypoint/keypointtravel/KeypointTravelApplication.java
+++ src/main/java/com/keypoint/keypointtravel/KeypointTravelApplication.java
@@ -2,11 +2,13 @@ package com.keypoint.keypointtravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 public class KeypointTravelApplication {
 
