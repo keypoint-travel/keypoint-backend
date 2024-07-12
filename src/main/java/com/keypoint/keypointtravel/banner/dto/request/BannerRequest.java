d1e40af131e1680b6841d228faba5f367b25eb27
Khdscor <khds_cor@naver.com>
2024-07-12 14:31:26 +0900

    feature: banner 생성 관련 request, useCase 구현

---
+++ src/main/java/com/keypoint/keypointtravel/banner/dto/request/BannerRequest.java
@@ -0,0 +1,29 @@
+package com.keypoint.keypointtravel.banner.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BannerRequest {
+
+    @NotEmpty(message = "제목을 입력해주세요.")
+    @NotBlank(message = "제목을 입력해주세요.")
+    @Size(max = 20, message = "제목의 최대길이를 초과하였습니다.")
+    private String thumbnailTitle;
+
+    @NotEmpty(message = "이미지 URL을 입력해주세요.")
+    @NotBlank(message = "이미지 URL을 입력해주세요.")
+    private String thumbnailImage;
+
+    private Long contentId;
+    private String region;
+    private String tourType;
+    private String cat1;
+    private String cat2;
+    private String cat3;
+
+}
---
+++ src/main/java/com/keypoint/keypointtravel/banner/dto/useCase/SaveUseCase.java
@@ -0,0 +1,40 @@
+package com.keypoint.keypointtravel.banner.dto.useCase;
+
+import com.keypoint.keypointtravel.banner.dto.request.BannerRequest;
+import com.keypoint.keypointtravel.global.enumType.banner.AreaCode;
+import com.keypoint.keypointtravel.global.enumType.banner.BannerCode;
+import com.keypoint.keypointtravel.global.enumType.banner.ContentType;
+import com.keypoint.keypointtravel.global.enumType.banner.LargeCategory;
+import com.keypoint.keypointtravel.global.enumType.banner.MiddleCategory;
+import com.keypoint.keypointtravel.global.enumType.banner.SmallCategory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SaveUseCase {
+
+    private Long contentId;
+    private AreaCode areaCode;
+    private LargeCategory cat1;
+    private MiddleCategory cat2;
+    private SmallCategory cat3;
+    private ContentType contentType;
+    private String thumbnailTitle;
+    private String thumbnailImage;
+
+    public static SaveUseCase from(BannerRequest request){
+        return SaveUseCase.builder()
+            .contentId(request.getContentId())
+            .areaCode(BannerCode.getConstant(AreaCode.class, request.getRegion()))
+            .cat1(BannerCode.getConstant(LargeCategory.class, request.getCat1()))
+            .cat2(BannerCode.getConstant(MiddleCategory.class, request.getCat2()))
+            .cat3(BannerCode.getConstant(SmallCategory.class, request.getCat3()))
+            .contentType(BannerCode.getConstant(ContentType.class, request.getTourType()))
+            .thumbnailTitle(request.getThumbnailTitle())
+            .thumbnailImage(request.getThumbnailImage())
+            .build();
+    }
+}
