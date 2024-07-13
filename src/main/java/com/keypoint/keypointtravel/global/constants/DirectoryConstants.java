186c083638130bb19f0687e6285caf8d82dd0d5e
chea-young <lchy0413@gmail.com>
2024-07-13 22:08:32 +0900

    [Feature] Add function to upload and delete file in s3 (#26)

---
+++ src/main/java/com/keypoint/keypointtravel/global/constants/DirectoryConstants.java
@@ -0,0 +1,5 @@
+package com.keypoint.keypointtravel.global.constants;
+
+public class DirectoryConstants {
+
+}
--- src/main/java/com/keypoint/keypointtravel/global/enumType/error/CommonErrorCode.java
+++ src/main/java/com/keypoint/keypointtravel/global/enumType/error/CommonErrorCode.java
@@ -10,6 +10,7 @@ public enum CommonErrorCode implements ErrorCode {
     INVALID_REQUEST_DATA("001_INVALID_REQUEST_DATA", "유효하지 않은 데이터 입니다"),
     CRYPTOGRAPHY_FAILED("002_CRYPTOGRAPHY_FAILED", "암호화/복호화에 실패하셨습니다."),
     OPEN_API_REQUEST_FAIL("003_OPEN_API_REQUEST_FAIL", "외부 API 요청에 실패하였습니다."),
+    FAIL_TO_CONVERT_FILE("004_FAIL_TO_CONVERT_FILE", "파일을 변환하는데 실패하였습니다."),
 
     ACCESS_DENIED_ACCOUNT("301_ACCESS_DENIED_ACCOUNT", "접근 권한이 없는 요청입니다.");
     private final String code;
