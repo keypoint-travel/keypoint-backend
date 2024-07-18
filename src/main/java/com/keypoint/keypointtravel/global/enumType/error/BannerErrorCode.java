fec632ec9a28de6ddbd47dc249bd221344438551
khdscor <khds_cor@naver.com>
2024-07-18 09:13:07 +0900

    test: 배너 좋아요 변경 비즈니스 로직 테스트 구현

--- src/main/java/com/keypoint/keypointtravel/global/enumType/error/BannerErrorCode.java
+++ src/main/java/com/keypoint/keypointtravel/global/enumType/error/BannerErrorCode.java
@@ -9,7 +9,8 @@ public enum BannerErrorCode implements ErrorCode {
 
     REQUEST_DATA_MISMATCH("001_REQUEST_DATA_MISMATCH", "해당하는 타입이 존재하지 않습니다"),
     NOT_EXISTED_BANNER("002_NOT_EXISTED_BANNER", "존재하지 않는 배너입니다."),
-    NOT_EXISTED_COMMENT("003_NOT_EXISTED_COMMENT", "댓글이 존재하지 않거나 작성자가 아닙니다.");
+    NOT_EXISTED_COMMENT("003_NOT_EXISTED_COMMENT", "댓글이 존재하지 않거나 작성자가 아닙니다."),
+    NOT_EXISTED_LIKE("003_NOT_EXISTED_LIKE", "이미 좋아요를 취소하였거나 누르지 않았습니다.");
 
     private final String code;
     private final String msg;
