b17c2e9e632e7a8c97854ffab731e34e75b56154
chea-young <lchy0413@gmail.com>
2024-07-20 14:53:07 +0900

    [Feature] Add CORS setting (#30)

---
+++ src/main/java/com/keypoint/keypointtravel/global/config/CORSConfig.java
@@ -0,0 +1,44 @@
+package com.keypoint.keypointtravel.global.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CORSConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(
+            Arrays.asList(
+                "http://localhost:9000",
+                "http://localhost:8000",
+                "http://192.168.1.8:8000",
+                "http://192.168.1.8:9000",
+                "http://192.168.1.8:9500",
+                "http://10.0.2.2:9500",
+                "http://10.0.2.2:9000",
+                "http://10.0.2.2:8000",
+                "capacitor://",
+                "capacitor://localhost",
+                "https://m.keypointtravel.com",
+                "https://m.keypointtravel.com",
+                "https://m-dev.keypointtravel.com",
+                "https://appleid.apple.com")
+        );
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(
+            Arrays.asList("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
