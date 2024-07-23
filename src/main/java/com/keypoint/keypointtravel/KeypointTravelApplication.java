e9a9e2fc75ade23fd55db6caaf0b9aeac082d0bb
chea-young <lchy0413@gmail.com>
2024-07-23 23:04:07 +0900

    [Feature] Set UTC about the datetime type (#29)

--- src/main/java/com/keypoint/keypointtravel/KeypointTravelApplication.java
+++ src/main/java/com/keypoint/keypointtravel/KeypointTravelApplication.java
@@ -1,5 +1,7 @@
 package com.keypoint.keypointtravel;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,4 +14,8 @@ public class KeypointTravelApplication {
         SpringApplication.run(KeypointTravelApplication.class, args);
     }
 
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
