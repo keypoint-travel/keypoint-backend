ce1e904ec17b795c4352de4d2e997e70c59d76e5
chea-young <lchy0413@gmail.com>
2024-07-26 00:48:41 +0900

    [Feature] Add api to update language and update member profile (#38)

--- src/main/java/com/keypoint/keypointtravel/member/controller/ReadMemberController.java
+++ src/main/java/com/keypoint/keypointtravel/member/controller/ReadMemberController.java
@@ -1,5 +1,6 @@
 package com.keypoint.keypointtravel.member.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/keypoint/keypointtravel/uploadFile/service/UploadFileService.java
+++ src/main/java/com/keypoint/keypointtravel/uploadFile/service/UploadFileService.java
@@ -1,16 +1,13 @@
 package com.keypoint.keypointtravel.uploadFile.service;
 
 import com.keypoint.keypointtravel.external.aws.service.S3Service;
-import com.keypoint.keypointtravel.global.enumType.error.MemberErrorCode;
-import com.keypoint.keypointtravel.global.exception.GeneralException;
-import com.keypoint.keypointtravel.member.dto.dto.CommonMemberDTO;
-import com.keypoint.keypointtravel.member.dto.useCase.EmailUseCase;
-import com.keypoint.keypointtravel.member.repository.MemberRepository;
+import com.keypoint.keypointtravel.global.entity.UploadFile;
 import com.keypoint.keypointtravel.uploadFile.repository.UploadFileRepository;
-
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Transactional(readOnly = true)
@@ -22,13 +19,32 @@ public class UploadFileService {
 
     /**
      * 파일을 S3에 업로드 후, UploadFile에 정보를 업데이트하는 함수
-     * 
-     * @param file
-     * @return
+     *
+     * @param file          s3에 업로드할 파일
+     * @param directoryName 파일을 업로드할 폴더 이름
+     * @return 저장된 UploadFile 의 id
+     * @throws IOException
      */
     @Transactional
-    public Long saveUploadFile(MultipartFile file, String directroy) {
-        String fileName = s3Service.uploadFileInS3(file, directroy);
+    public Long saveUploadFile(MultipartFile file, String directoryName) throws IOException {
+        // 1. s3 파일 업로드
+        String fileName = s3Service.uploadFileInS3(file, directoryName);
 
+        // 2. UploadFile 저장
+        UploadFile uploadFile = UploadFile.of(fileName, file);
+        uploadFileRepository.save(uploadFile);
+
+        return uploadFile.getId();
+    }
+
+
+    /**
+     * UploadFile 삭제 함수 (isDeleted를 true로 변경)
+     *
+     * @param uploadFileId 삭제할 UploadFile 의 id
+     */
+    @Transactional
+    public void deleteUploadFile(Long uploadFileId) {
+        uploadFileRepository.updateIsDeletedTrue(uploadFileId);
     }
 }
