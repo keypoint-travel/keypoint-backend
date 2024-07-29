7ea4b6a9be2259e501c5448ff30dd55c7b5ec736
khdscor <khds_cor@naver.com>
2024-07-29 10:13:48 +0900

    feature: 광고 배너 생성 api 구현

--- src/main/java/com/keypoint/keypointtravel/banner/controller/advertisement/AdvertisementBannerController.java
+++ src/main/java/com/keypoint/keypointtravel/banner/controller/advertisement/AdvertisementBannerController.java
@@ -1,16 +1,17 @@
 package com.keypoint.keypointtravel.banner.controller.advertisement;
 
+import com.keypoint.keypointtravel.banner.dto.request.AdvertisementRequest;
 import com.keypoint.keypointtravel.banner.dto.response.ImageUrlResponse;
+import com.keypoint.keypointtravel.banner.dto.useCase.AdvertisementUseCase;
 import com.keypoint.keypointtravel.banner.dto.useCase.ImageUseCase;
 import com.keypoint.keypointtravel.banner.service.AdvertisementBannerService;
 import com.keypoint.keypointtravel.global.config.security.CustomUserDetails;
 import com.keypoint.keypointtravel.global.dto.response.APIResponseEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -31,5 +32,20 @@ public class AdvertisementBannerController {
             .message("이미지 파일 url 변환 성공")
             .data(response)
             .build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveAdvertisementBanner(
+        @RequestPart(value = "thumbnailImage", required = false) MultipartFile thumbnailImage,
+        @RequestPart(value = "detailImage", required = false) MultipartFile detailImage,
+        @RequestPart(value = "detail") AdvertisementRequest request,
+        @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        //todo: 관리자 인증 로직 추가 예정
+        AdvertisementUseCase useCase = new AdvertisementUseCase(
+            thumbnailImage, detailImage, request.getTitle(), request.getContent());
+
+        advertisementBannerService.saveAdvertisementBanner(useCase);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
---
+++ src/main/java/com/keypoint/keypointtravel/banner/dto/request/AdvertisementRequest.java
@@ -0,0 +1,11 @@
+package com.keypoint.keypointtravel.banner.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AdvertisementRequest {
+    private String title;
+    private String content;
+}
