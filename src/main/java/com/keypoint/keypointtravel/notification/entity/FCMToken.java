6c1ea32fd13d41282e61e80ea5b54600389982a8
chea-young <lchy0413@gmail.com>
2024-08-02 21:31:47 +0900

    [Feature] Add entity, repository, service about fcm_token ( #46)

---
+++ src/main/java/com/keypoint/keypointtravel/notification/entity/FCMToken.java
@@ -0,0 +1,36 @@
+package com.keypoint.keypointtravel.notification.entity;
+
+import com.keypoint.keypointtravel.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@Table(name = "fcm_token")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FCMToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fcm_token_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Comment("fcm push 토큰")
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
