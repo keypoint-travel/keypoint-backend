804d7898a12646159dc558b648e9d34759e4351c
chea-young <lchy0413@gmail.com>
2024-08-04 13:02:43 +0900

    [Refactor] modify push notification structure (#54) - PushNotificationEvent와 event를 받았을 때 데이터 처리를 push type에 따라서 처리할 수 있도록 수정

---
+++ src/main/java/com/keypoint/keypointtravel/notification/event/pushNotification/PushNotificationEvent.java
@@ -0,0 +1,17 @@
+package com.keypoint.keypointtravel.notification.event.pushNotification;
+
+import com.keypoint.keypointtravel.global.enumType.notification.PushNotificationType;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public abstract class PushNotificationEvent {
+
+    private PushNotificationType pushNotificationType; // 푸시 알림 타입
+
+    private List<Long> userId; // 푸시 알림을 보낼 사용자 idc
+    
+    public abstract Object getAdditionalData();
+}
