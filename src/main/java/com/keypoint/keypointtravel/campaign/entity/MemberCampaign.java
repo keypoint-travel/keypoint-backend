4493c408ea9802942ea891bfe93ba1322386bc1e
khdscor <khds_cor@naver.com>
2024-08-07 14:10:35 +0900

    feature: 회원-캠페인 연결 테이블 구현

---
+++ src/main/java/com/keypoint/keypointtravel/campaign/entity/MemberCampaign.java
@@ -0,0 +1,28 @@
+package com.keypoint.keypointtravel.campaign.entity;
+
+import com.keypoint.keypointtravel.global.entity.BaseEntity;
+import com.keypoint.keypointtravel.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "member_campaign")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberCampaign extends BaseEntity {
+
+    @Id
+    @Column(name="member_campaign_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "campaign_id")
+    private Campaign campaign;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
