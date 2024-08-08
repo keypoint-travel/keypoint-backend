32e358d50bcf23c3644df9df72a8c87edff453a9
khdscor <khds_cor@naver.com>
2024-08-08 14:33:21 +0900

    refactor: 사용자 추천 배너 응답 시 이미지, 카테고리 1 - 3 응답 값 추가

--- src/main/java/com/keypoint/keypointtravel/banner/dto/response/RecommendationResponse.java
+++ src/main/java/com/keypoint/keypointtravel/banner/dto/response/RecommendationResponse.java
@@ -2,6 +2,10 @@ package com.keypoint.keypointtravel.banner.dto.response;
 
 import com.keypoint.keypointtravel.banner.dto.response.commonBanner.AroundTourism;
 import com.keypoint.keypointtravel.banner.dto.useCase.tourListUseCase.Item;
+import com.keypoint.keypointtravel.global.enumType.banner.BannerCode;
+import com.keypoint.keypointtravel.global.enumType.banner.LargeCategory;
+import com.keypoint.keypointtravel.global.enumType.banner.MiddleCategory;
+import com.keypoint.keypointtravel.global.enumType.banner.SmallCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,7 +23,11 @@ public class RecommendationResponse {
     private String address2;
     private String latitude;
     private String longitude;
-    private List<AroundTourism> arounds;
+    private String image;
+    private String cat1;
+    private String cat2;
+    private String cat3;
+    private List<AroundTourism> around;
 
     public static RecommendationResponse from(List<Item> items){
         return RecommendationResponse.builder()
@@ -29,7 +37,11 @@ public class RecommendationResponse {
             .address2(items.get(0).getAddr2())
             .latitude(items.get(0).getMapy())
             .longitude(items.get(0).getMapx())
-            .arounds(items.stream()
+            .image(items.get(0).getFirstimage())
+            .cat1(BannerCode.getDescription(LargeCategory.class, items.get(0).getCat1()))
+            .cat2(BannerCode.getDescription(MiddleCategory.class, items.get(0).getCat2()))
+            .cat3(BannerCode.getDescription(SmallCategory.class, items.get(0).getCat3()))
+            .around(items.stream()
                 .skip(1)
                 .map(AroundTourism::from).toList())
             .build();
