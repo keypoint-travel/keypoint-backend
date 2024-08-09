4c11ca1929a3baaa9cff6647cbb77aeda5b8ef3b
khdscor <khds_cor@naver.com>
2024-08-09 18:14:41 +0900

    refactor: 배너, 배너 내용 엔티티 생성

--- src/main/java/com/keypoint/keypointtravel/banner/entity/Banner.java
+++ src/main/java/com/keypoint/keypointtravel/banner/entity/Banner.java
@@ -15,46 +15,13 @@ import java.util.List;
 public class Banner extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "banner_id")
     private Long id;
-
-    @Column
-    private String name;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private AreaCode areaCode;
-
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private LargeCategory cat1;
-
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private MiddleCategory cat2;
-
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private SmallCategory cat3;
-
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private ContentType contentType;
-
-    @Column(nullable = false)
-    private String mainTitle;
-
-    @Column(nullable = false)
-    private String subTitle;
-
-    @Column
-    private String thumbnailImage;
-
-    @Column
-    private String address1;
-
-    @Column
-    private String address2;
 
     @Column
     private Double latitude;
@@ -63,7 +30,10 @@ public class Banner extends BaseEntity {
     private Double longitude;
 
     @Column(nullable = false)
-    private boolean isExposed; // 사용자에게 노출 여부
+    private boolean isDeleted;
+
+    @OneToMany(mappedBy = "banner", orphanRemoval = true)
+    private List<BannerContent> bannerContents = new ArrayList<>();
 
     @OneToMany(mappedBy = "banner", orphanRemoval = true)
     private List<BannerComment> comments = new ArrayList<>();
@@ -72,23 +42,10 @@ public class Banner extends BaseEntity {
     private List<BannerLike> bannerLikes = new ArrayList<>();
 
     @Builder
-    public Banner(Long id, String name, AreaCode areaCode, LargeCategory cat1, MiddleCategory cat2, SmallCategory cat3,
-                  ContentType contentType, String mainTitle, String subTitle, String thumbnailImage, String address1, String address2,
-                  Double latitude, Double longitude, boolean isExposed) {
-        this.id = id;
-        this.name = name;
+    public Banner(AreaCode areaCode, Double latitude, Double longitude, boolean isDeleted) {
         this.areaCode = areaCode;
-        this.cat1 = cat1;
-        this.cat2 = cat2;
-        this.cat3 = cat3;
-        this.contentType = contentType;
-        this.mainTitle = mainTitle;
-        this.subTitle = subTitle;
-        this.thumbnailImage = thumbnailImage;
-        this.address1 = address1;
-        this.address2 = address2;
         this.latitude = latitude;
         this.longitude = longitude;
-        this.isExposed = isExposed;
+        this.isDeleted = isDeleted;
     }
 }
