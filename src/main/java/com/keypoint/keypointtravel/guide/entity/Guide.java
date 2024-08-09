86067843eebe4ae08b8b75458037c1265ff4794f
chea-young <lchy0413@gmail.com>
2024-08-09 11:52:53 +0900

    [Feature] Add entity about guide (#72)

---
+++ src/main/java/com/keypoint/keypointtravel/guide/entity/Guide.java
@@ -0,0 +1,33 @@
+package com.keypoint.keypointtravel.guide.entity;
+
+import com.keypoint.keypointtravel.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "guide")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Guide extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "guide_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Long thumbnailImageId;
+
+    @Column(nullable = false)
+    private int order;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+}
