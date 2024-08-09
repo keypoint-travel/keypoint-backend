95e9dd93be1b201e0b1de9d79603bb8774bc70f9
khdscor <khds_cor@naver.com>
2024-08-09 15:15:49 +0900

    feature: 한국관광공사 영문, 일문 버전 코드 별 areaCode 수정

--- src/main/java/com/keypoint/keypointtravel/global/enumType/banner/AreaCode.java
+++ src/main/java/com/keypoint/keypointtravel/global/enumType/banner/AreaCode.java
@@ -7,24 +7,26 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AreaCode implements BannerCode {
-    SEOUL("1", "서울"),
-    INCHEON("2", "인천"),
-    DAEJEON("3", "대전"),
-    DAEGU("4", "대구"),
-    GWANGJU("5", "광주"),
-    BUSAN("6", "부산"),
-    ULSAN("7", "울산"),
-    SEJONG("8", "세종"),
-    GYEONGGI("31", "경기"),
-    GANGWON("32", "강원"),
-    CHUNGBUK("33", "충북"),
-    CHUNGNAM("34", "충남"),
-    JEONBUK("35", "경북"),
-    JEONNAM("36", "경남"),
-    GYEONGBUK("37", "전북"),
-    GYEONGNAM("38", "전남"),
-    JEJU("39", "제주");
+    SEOUL("1", "서울", "Seoul","ソウル特別市"),
+    INCHEON("2", "인천", "Incheon","仁川広域市"),
+    DAEJEON("3", "대전", "Daejeon","大田広域市"),
+    DAEGU("4", "대구", "Daegu","大邱広域市"),
+    GWANGJU("5", "광주", "Gwangju","光州広域市"),
+    BUSAN("6", "부산", "Busan","釜山広域市"),
+    ULSAN("7", "울산", "Ulsan","蔚山広域市"),
+    SEJONG("8", "세종", "Sejong","世宗特別自治市"),
+    GYEONGGI("31", "경기", "Gyeonggi-do","京畿道"),
+    GANGWON("32", "강원", "Gangwon-do","江原道"),
+    CHUNGBUK("33", "충북", "Chungcheongbuk-do","忠清北道"),
+    CHUNGNAM("34", "충남", "Chungcheongnam-do","忠清南道"),
+    JEONBUK("35", "경북", "Gyeongsangbuk-do","慶尚北道"),
+    JEONNAM("36", "경남", "Gyeongsangnam-do","慶尚南道"),
+    GYEONGBUK("37", "전북", "Jeonbuk-do", "チョンブク特別自治道"),
+    GYEONGNAM("38", "전남", "Jeollanam-do", "全羅南道"),
+    JEJU("39", "제주", "Jeju-do", "済州特別自治道");
 
     private final String code;
     private final String description;
+    private final String engDescription;
+    private final String japDescription;
 }
