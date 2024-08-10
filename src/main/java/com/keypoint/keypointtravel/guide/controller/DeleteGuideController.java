7063f91714c025133e7111bc1f99068da9553a05
chea-young <lchy0413@gmail.com>
2024-08-10 23:19:10 +0900

    [Feature] Add dto about DeleteGuideController (#72)

--- src/main/java/com/keypoint/keypointtravel/guide/controller/DeleteGuideController.java
+++ src/main/java/com/keypoint/keypointtravel/guide/controller/DeleteGuideController.java
@@ -1,35 +1,36 @@
 package com.keypoint.keypointtravel.guide.controller;
 
+import com.keypoint.keypointtravel.global.dto.response.APIResponseEntity;
+import com.keypoint.keypointtravel.guide.dto.useCase.DeleteGuideGuideUseCase;
+import com.keypoint.keypointtravel.guide.dto.useCase.DeleteGuideTranslationUseCase;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.keypoint.keypointtravel.global.dto.response.APIResponseEntity;
-
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/guides")
 public class DeleteGuideController {
-    @DeleteMapping("")
-    public APIResponseEntity<?> deleteGuide(
-        @RequestParam(value = "guide-id") Long[] guideIds
-    ) {
 
-        return APIResponseEntity.<?>builder()
+    @DeleteMapping("")
+    public APIResponseEntity<Void> deleteGuide(
+        @RequestParam(value = "ids") Long[] ids
+    ) {
+        DeleteGuideGuideUseCase useCase = DeleteGuideGuideUseCase.from(ids);
+        return APIResponseEntity.<Void>builder()
                 .message("이용 가이드 삭제 성공")
                 .build();
     }
     
     @GetMapping("{guideId}")
-    public APIResponseEntity<?> deleteGuideTranslation(
-        @RequestParam(value = "guide-translation-id") Long[] guideTranslationIds
+    public APIResponseEntity<Void> deleteGuideTranslation(
+        @RequestParam(value = "guide-translation-id") Long[] ids
         ) {
-
-        return APIResponseEntity.<?>builder()
+        DeleteGuideTranslationUseCase useCase = DeleteGuideTranslationUseCase.from(ids);
+        return APIResponseEntity.<Void>builder()
             .message("이용 가이드 삭제 성공")
             .build();
     }
---
+++ src/main/java/com/keypoint/keypointtravel/guide/dto/useCase/DeleteGuideGuideUseCase.java
@@ -0,0 +1,15 @@
+package com.keypoint.keypointtravel.guide.dto.useCase;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DeleteGuideGuideUseCase {
+
+  private Long[] guideIds;
+
+  public static DeleteGuideGuideUseCase from(Long[] guideId) {
+    return new DeleteGuideGuideUseCase(guideId);
+  }
+}
