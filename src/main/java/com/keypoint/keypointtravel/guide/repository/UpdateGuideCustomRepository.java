4f1ec5f0fe7012ce627d455bfcf2186e8288ecde
chea-young <lchy0413@gmail.com>
2024-08-12 18:01:28 +0900

    [Feature] Add DELETE api/v1/guides/{guideId} and api/v1/guides (#72)

--- src/main/java/com/keypoint/keypointtravel/guide/repository/UpdateGuideCustomRepository.java
+++ src/main/java/com/keypoint/keypointtravel/guide/repository/UpdateGuideCustomRepository.java
@@ -1,8 +1,14 @@
 package com.keypoint.keypointtravel.guide.repository;
 
+import com.keypoint.keypointtravel.guide.dto.useCase.DeleteGuideGuideUseCase;
+import com.keypoint.keypointtravel.guide.dto.useCase.DeleteGuideTranslationUseCase;
 import com.keypoint.keypointtravel.guide.dto.useCase.updateGuide.UpdateGuideUseCase;
 
 public interface UpdateGuideCustomRepository {
 
     void updateGuide(UpdateGuideUseCase useCase);
+
+    void deleteGuides(DeleteGuideGuideUseCase useCase);
+
+    void deleteGuideTranslations(DeleteGuideTranslationUseCase useCase);
 }
--- src/main/java/com/keypoint/keypointtravel/guide/service/DeleteGuideService.java
+++ src/main/java/com/keypoint/keypointtravel/guide/service/DeleteGuideService.java
@@ -1,15 +1,45 @@
 package com.keypoint.keypointtravel.guide.service;
 
+import com.keypoint.keypointtravel.global.exception.GeneralException;
+import com.keypoint.keypointtravel.guide.dto.useCase.DeleteGuideGuideUseCase;
+import com.keypoint.keypointtravel.guide.dto.useCase.DeleteGuideTranslationUseCase;
+import com.keypoint.keypointtravel.guide.repository.GuideRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.keypoint.keypointtravel.guide.repository.GuideRepository;
-
-import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class DeleteGuideService {
-     private final GuideRepository guideRepository;
+
+    private final GuideRepository guideRepository;
+
+    /**
+     * 이용 가이드 삭제 함수
+     *
+     * @param useCase
+     */
+    @Transactional
+    public void deleteGuides(DeleteGuideGuideUseCase useCase) {
+        try {
+            guideRepository.deleteGuides(useCase);
+        } catch (Exception ex) {
+            throw new GeneralException(ex);
+        }
+    }
+
+    /**
+     * 이용 가이드 번역물 삭제 함수
+     *
+     * @param useCase
+     */
+    @Transactional
+    public void deleteGuideTranslations(DeleteGuideTranslationUseCase useCase) {
+        try {
+            guideRepository.deleteGuideTranslations(useCase);
+        } catch (Exception ex) {
+            throw new GeneralException(ex);
+        }
+    }
 }
