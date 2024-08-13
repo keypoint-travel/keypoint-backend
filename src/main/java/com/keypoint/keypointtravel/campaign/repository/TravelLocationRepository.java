b856235dd2dea8abf14c4185f6762d675876cd54
khdscor <khds_cor@naver.com>
2024-08-13 15:28:49 +0900

    feature: 캠페인 Repository 구현

---
+++ src/main/java/com/keypoint/keypointtravel/campaign/repository/TravelLocationRepository.java
@@ -0,0 +1,7 @@
+package com.keypoint.keypointtravel.campaign.repository;
+
+import com.keypoint.keypointtravel.campaign.entity.TravelLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TravelLocationRepository extends JpaRepository<TravelLocation, Long> {
+}
