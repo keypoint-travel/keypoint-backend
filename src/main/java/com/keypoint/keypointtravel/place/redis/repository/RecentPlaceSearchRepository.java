3af3761e6e3518ef71b9a0e5aeb82e8e2a6bf03c
chea-young <lchy0413@gmail.com>
2024-08-16 02:01:32 +0900

    [Feature] Add /api/v1/places/search-history (#79)

--- src/main/java/com/keypoint/keypointtravel/place/redis/repository/RecentPlaceSearchRepository.java
+++ src/main/java/com/keypoint/keypointtravel/place/redis/repository/RecentPlaceSearchRepository.java
@@ -1,11 +1,16 @@
 package com.keypoint.keypointtravel.place.redis.repository;
 
-import com.keypoint.keypointtravel.place.controller.RecentPlaceSearchController;
+import com.keypoint.keypointtravel.place.redis.entity.RecentPlaceSearch;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecentPlaceSearchRepository extends
-    CrudRepository<RecentPlaceSearchController, String> {
+    CrudRepository<RecentPlaceSearch, String> {
 
+    List<RecentPlaceSearch> findByMemberId(Long memberId);
+
+    Optional<RecentPlaceSearch> findByMemberIdAndSearchWord(Long memberId, String searchWord);
 }
--- src/main/java/com/keypoint/keypointtravel/place/redis/service/RecentPlaceSearchService.java
+++ src/main/java/com/keypoint/keypointtravel/place/redis/service/RecentPlaceSearchService.java
@@ -1,10 +1,12 @@
 package com.keypoint.keypointtravel.place.redis.service;
 
+import com.keypoint.keypointtravel.global.constants.PlaceConstants;
 import com.keypoint.keypointtravel.global.exception.GeneralException;
 import com.keypoint.keypointtravel.member.dto.useCase.MemberIdUseCase;
 import com.keypoint.keypointtravel.place.dto.response.ReadPlaceSearchHistoryResponse;
 import com.keypoint.keypointtravel.place.dto.useCase.DeleteRecentPlaceSearchUseCase;
 import com.keypoint.keypointtravel.place.dto.useCase.PlaceSearchUseCase;
+import com.keypoint.keypointtravel.place.redis.entity.RecentPlaceSearch;
 import com.keypoint.keypointtravel.place.redis.repository.RecentPlaceSearchRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,16 +18,35 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RecentPlaceSearchService {
 
-    private RecentPlaceSearchRepository recentPlaceSearchRepository;
+    private final RecentPlaceSearchRepository recentPlaceSearchRepository;
 
     /**
      * 최근 장소 검색어 추가
      *
      * @param useCase
      */
+    @Transactional
     public void addSearchWord(PlaceSearchUseCase useCase) {
         try {
+            String searchWord = useCase.getSearchWord();
+            Long memberId = useCase.getMemberId();
 
+            // 1. 이미 존재하면 날짜 업데이트, 없으면 저장
+            RecentPlaceSearch recentPlaceSearch = recentPlaceSearchRepository
+                .findByMemberIdAndSearchWord(memberId, searchWord)
+                .orElse(RecentPlaceSearch.of(memberId, searchWord));
+
+            recentPlaceSearch.setModifyAt();
+            recentPlaceSearchRepository.save(recentPlaceSearch);
+
+            // 3. 5개가 넘는 경우, 삭제
+            List<RecentPlaceSearch> recentPlaceSearches = recentPlaceSearchRepository.findByMemberId(
+                memberId);
+            if (recentPlaceSearches.size() > PlaceConstants.MAX_PLACE_SEARCH_WORD_CNT) {
+                recentPlaceSearchRepository.delete(
+                    recentPlaceSearches.get(0)
+                );
+            }
         } catch (Exception ex) {
             throw new GeneralException(ex);
         }
