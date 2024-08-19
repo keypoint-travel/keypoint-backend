aa285a21dd772bba1a1549bb7486162161d842eb
Khdscor <khds_cor@naver.com>
2024-08-19 22:49:46 +0900

    refactor: 이메일 전송 로직 리펙토링

--- src/main/java/com/keypoint/keypointtravel/global/utils/EmailUtils.java
+++ src/main/java/com/keypoint/keypointtravel/global/utils/EmailUtils.java
@@ -45,23 +45,12 @@ public class EmailUtils {
         EmailTemplate template,
         Map<String, String> emailContent
     ) {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-
         try {
-            MimeMessageHelper msgHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            // 이메일 전송을 위한 MimeMessageHelper 객체 생성
+            MimeMessageHelper msgHelper = prepareMessage(template, emailContent, false);
             msgHelper.setTo(receiver);
-            msgHelper.setSubject(template.getSubject());
-
-            // 템플릿에 매핑된 값을 설정
-            Context context = new Context();
-            emailContent.forEach(context::setVariable);
-
-            // 템플릿을 처리하여 이메일 본문 생성
-            String emailBody = templateEngine.process(template.getTemplate(), context);
-            msgHelper.setText(emailBody, true);
-
             // 이메일 전송
-            javaMailSender.send(mimeMessage);
+            javaMailSender.send(msgHelper.getMimeMessage());
         } catch (MessagingException e) {
             throw new GeneralException(CommonErrorCode.FAIL_TO_SEND_EMAIL);
         }
@@ -70,7 +59,7 @@ public class EmailUtils {
     /**
      * 이메일 다중 전송 함수
      *
-     * @param receiver     받는 사람
+     * @param receivers    받는 사람
      * @param template     이메일 template
      * @param emailContent 이메일 매핑 내용
      * @return 이메일 성공 여부
@@ -80,67 +69,92 @@ public class EmailUtils {
         EmailTemplate template,
         Map<String, String> emailContent
     ) {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-
         try {
-            MimeMessageHelper msgHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            // 이메일 전송을 위한 MimeMessageHelper 객체 생성
+            MimeMessageHelper msgHelper = prepareMessage(template, emailContent, false);
             msgHelper.setTo(receivers.toArray(new String[receivers.size()]));
-            msgHelper.setSubject(template.getSubject());
-
-            // 템플릿에 매핑된 값을 설정
-            Context context = new Context();
-            emailContent.forEach(context::setVariable);
-
-            // 템플릿을 처리하여 이메일 본문 생성
-            String emailBody = templateEngine.process(template.getTemplate(), context);
-            msgHelper.setText(emailBody, true);
-
             // 이메일 전송
-            javaMailSender.send(mimeMessage);
+            javaMailSender.send(msgHelper.getMimeMessage());
+
         } catch (MessagingException e) {
             throw new GeneralException(CommonErrorCode.FAIL_TO_SEND_EMAIL);
         }
     }
 
     /**
-     * 이메일 단일 전송 함수
+     * 이메일 단일 전송 함수(이미지 포함)
      *
      * @param receiver     받는 사람
      * @param template     이메일 template
      * @param emailContent 이메일 매핑 내용
+     * @param imagePaths   이메일 이미지 매핑 내용
      * @return 이메일 성공 여부
      */
     public static void sendSingleEmailWithImages(
         String receiver,
         EmailTemplate template,
-        Map<String, String> emailContent,
-        List<String> imagePaths
+        Map<String, String> emailContent, List<String> imagePaths
     ) {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         try {
-            MimeMessageHelper msgHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            // 이메일 전송을 위한 MimeMessageHelper 객체 생성
+            MimeMessageHelper msgHelper = prepareMessage(template, emailContent, true);
             msgHelper.setTo(receiver);
-            msgHelper.setSubject(template.getSubject());
-
-            // 템플릿에 매핑된 값을 설정
-            Context context = new Context();
-            emailContent.forEach(context::setVariable);
-
-            // 템플릿을 처리하여 이메일 본문 생성
-            String emailBody = templateEngine.process(template.getTemplate(), context);
-            msgHelper.setText(emailBody, true);
-
             // 이미지 리스트를 반복하고 각 이미지에 대해 addInline 메소드를 호출
             for (int i = 0; i < imagePaths.size(); i++) {
                 String imagePath = imagePaths.get(i);
                 String contentId = "image" + (i + 1);
                 msgHelper.addInline(contentId, new ClassPathResource(imagePath));
             }
-
             // 이메일 전송
-            javaMailSender.send(mimeMessage);
+            javaMailSender.send(msgHelper.getMimeMessage());
         } catch (MessagingException e) {
             throw new GeneralException(CommonErrorCode.FAIL_TO_SEND_EMAIL);
         }
+    }
+
+    /**
+     * 이메일 다중 전송 함수(이미지 포함)
+     *
+     * @param receivers    받는 사람
+     * @param template     이메일 template
+     * @param emailContent 이메일 매핑 내용
+     * @param imagePaths   이메일 이미지 매핑 내용
+     * @return 이메일 성공 여부
+     */
+    public static void sendMultiEmailWithImages(
+        List<String> receivers,
+        EmailTemplate template,
+        Map<String, String> emailContent, List<String> imagePaths
+    ) {
+        try {
+            // 이메일 전송을 위한 MimeMessageHelper 객체 생성
+            MimeMessageHelper msgHelper = prepareMessage(template, emailContent, true);
+            msgHelper.setTo(receivers.toArray(new String[receivers.size()]));
+            // 이미지 리스트를 반복하고 각 이미지에 대해 addInline 메소드를 호출
+            for (int i = 0; i < imagePaths.size(); i++) {
+                String imagePath = imagePaths.get(i);
+                String contentId = "image" + (i + 1);
+                msgHelper.addInline(contentId, new ClassPathResource(imagePath));
+            }
+            // 이메일 전송
+            javaMailSender.send(msgHelper.getMimeMessage());
+        } catch (MessagingException e) {
+            throw new GeneralException(CommonErrorCode.FAIL_TO_SEND_EMAIL);
+        }
+    }
+
+    private static MimeMessageHelper prepareMessage(EmailTemplate template,
+        Map<String, String> emailContent, boolean isMultiFile) throws MessagingException {
+        // 이메일 전송을 위한 MimeMessageHelper 객체 생성
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper msgHelper = new MimeMessageHelper(mimeMessage, isMultiFile, "UTF-8");
+        // 템플릿에 매핑된 값을 설정
+        msgHelper.setSubject(template.getSubject());
+        Context context = new Context();
+        emailContent.forEach(context::setVariable);
+        // 템플릿을 처리하여 이메일 본문 생성
+        String emailBody = templateEngine.process(template.getTemplate(), context);
+        msgHelper.setText(emailBody, true);
+        return msgHelper;
     }
 }
