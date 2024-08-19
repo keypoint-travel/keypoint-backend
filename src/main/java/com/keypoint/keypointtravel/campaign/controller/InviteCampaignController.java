da7b2bf443529d69bd4a087b37a6ceac15bdb98f
Khdscor <khds_cor@naver.com>
2024-08-19 19:42:57 +0900

    feature: 캠페인 이메일 초대 api 구현

---
+++ src/main/java/com/keypoint/keypointtravel/campaign/controller/InviteCampaignController.java
@@ -0,0 +1,38 @@
+package com.keypoint.keypointtravel.campaign.controller;
+
+import com.keypoint.keypointtravel.campaign.dto.request.InviteByEmailRequest;
+import com.keypoint.keypointtravel.campaign.dto.useCase.InviteByEmailUseCase;
+import com.keypoint.keypointtravel.campaign.service.InviteCampaignService;
+import com.keypoint.keypointtravel.global.config.security.CustomUserDetails;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/campaigns/invitation")
+@RequiredArgsConstructor
+public class InviteCampaignController {
+
+    private final InviteCampaignService inviteCampaignService;
+
+    @PreAuthorize("hasRole('ROLE_CERTIFIED_USER')")
+    @PostMapping("/email")
+    public ResponseEntity<Void> inviteByEmail(
+        @RequestBody @Valid InviteByEmailRequest request,
+        @AuthenticationPrincipal CustomUserDetails userDetails) {
+        InviteByEmailUseCase useCase = new InviteByEmailUseCase(
+            request.getEmail(),
+            userDetails.getId(),
+            request.getCampaignId());
+        inviteCampaignService.validateInvitation(useCase);
+        inviteCampaignService.sendEmail(useCase);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
