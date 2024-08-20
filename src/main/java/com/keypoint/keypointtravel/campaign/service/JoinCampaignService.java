b93134d47b0e16e37bdf2c00a299990c50a0f570
Khdscor <khds_cor@naver.com>
2024-08-20 18:14:16 +0900

    feature : 초대 받은 이메일을 통한 캠페인 가입 비즈니스 로직 구현

---
+++ src/main/java/com/keypoint/keypointtravel/campaign/service/JoinCampaignService.java
@@ -0,0 +1,73 @@
+package com.keypoint.keypointtravel.campaign.service;
+
+import com.keypoint.keypointtravel.campaign.dto.dto.EmailInvitationHistory;
+import com.keypoint.keypointtravel.campaign.dto.useCase.JoinByEmailUseCase;
+import com.keypoint.keypointtravel.campaign.entity.Campaign;
+import com.keypoint.keypointtravel.campaign.entity.MemberCampaign;
+import com.keypoint.keypointtravel.campaign.repository.CampaignRepository;
+import com.keypoint.keypointtravel.campaign.repository.EmailInvitationHistoryRepository;
+import com.keypoint.keypointtravel.campaign.repository.MemberCampaignRepository;
+import com.keypoint.keypointtravel.friend.entity.Friend;
+import com.keypoint.keypointtravel.friend.repository.FriendRepository;
+import com.keypoint.keypointtravel.global.enumType.error.CampaignErrorCode;
+import com.keypoint.keypointtravel.global.exception.GeneralException;
+import com.keypoint.keypointtravel.member.entity.Member;
+import com.keypoint.keypointtravel.member.repository.member.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class JoinCampaignService {
+
+    private final CampaignRepository campaignRepository;
+
+    private final EmailInvitationHistoryRepository emailInvitationHistoryRepository;
+
+    private final MemberCampaignRepository memberCampaignRepository;
+
+    private final MemberRepository memberRepository;
+
+    private final FriendRepository friendRepository;
+
+    /**
+     * 이메일을 통한 캠페인 가입 함수
+     *
+     * @Param email, memberId, campaignId useCase
+     */
+    @Transactional
+    public void joinByEmail(JoinByEmailUseCase useCase) {
+        // redis 캠페인 초대 이메일 목록에 저장되어 있는지(만료되지 않았는지 확인)
+        List<EmailInvitationHistory> histories =
+            emailInvitationHistoryRepository.findByCampaignId(useCase.getCampaignId());
+        boolean isEmailExists = histories.stream()
+            .anyMatch(history -> history.getEmail().equals(useCase.getEmail()));
+        if (!isEmailExists) {
+            throw new GeneralException(CampaignErrorCode.EXPIRED_INVITE_EMAIL);
+        }
+        // 캠페인 리더 member 추출 & 진행 중인 캠페인인지 확인
+        MemberCampaign leader = campaignRepository.findCampaignLeader(useCase.getCampaignId());
+        // 회원 - 캠페인 태이블 추가(가입)
+        Member member = memberRepository.getReferenceById(useCase.getMemberId());
+        Campaign campaign = campaignRepository.getReferenceById(useCase.getCampaignId());
+        memberCampaignRepository.save(new MemberCampaign(campaign, member, false));
+
+        // 캠페인 리더와 친구관계 구축
+        friendRepository.save(buildFriend(leader.getMember(), member));
+        friendRepository.save(buildFriend(member, leader.getMember()));
+        // todo: redis 에서 삭제 로직
+        // todo: 이미 가입되어 있는지,
+        // todo: 이미 친구 인지
+        // todo : 대상자 및 캠페인 참여 인원들에게 알림 발송
+    }
+
+    private Friend buildFriend(Member findedMember, Member member) {
+        return Friend.builder()
+            .friendId(findedMember.getId())
+            .member(member)
+            .isDeleted(false)
+            .build();
+    }
+}
