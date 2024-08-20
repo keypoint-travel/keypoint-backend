d297a18d87e71660b8adb72d52a73afe24a9d8fc
chea-young <lchy0413@gmail.com>
2024-08-20 17:19:29 +0900

    [Feature] update dto about receipt (#80)

--- src/main/java/com/keypoint/keypointtravel/receipt/dto/response/receiptOCRResult/ReceiptOCRResult.java
+++ src/main/java/com/keypoint/keypointtravel/receipt/dto/response/receiptOCRResult/ReceiptOCRResult.java
@@ -1,5 +1,12 @@
 package com.keypoint.keypointtravel.receipt.dto.response.receiptOCRResult;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class ReceiptOCRResult {
-    
+    private String store;
+    private String storeAddress;
+    private LocalDateTime paidAt;
+    private long totalAccount;
+    private List<PaymentItemOCRResult> paymentItems;
 }
