a9eaedcfed0232c500d1d041ed3bd0a81b91b2fb
chea-young <lchy0413@gmail.com>
2024-08-21 00:09:47 +0900

    [Feature] Update ReceiptCategory enum (#80)

--- src/main/java/com/keypoint/keypointtravel/global/enumType/receipt/ReceiptCategory.java
+++ src/main/java/com/keypoint/keypointtravel/global/enumType/receipt/ReceiptCategory.java
@@ -6,7 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ReceiptCategory {
-    EXCEPT(1, "기타");
+    TRANSPORTATION(1, "교통비"),
+    ACCOMMODATION(2, "숙박비"),
+    SHOPPING(3, "쇼핑"),
+    FOOD(4, "식비"),
+    OTHER(5, "기타");
 
     private final int code;
     private final String description;
