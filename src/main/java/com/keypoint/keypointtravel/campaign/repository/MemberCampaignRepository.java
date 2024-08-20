0865c56337920e340c7e72eefd66b782cf3e742f
khdscor <khds_cor@naver.com>
2024-08-20 20:39:33 +0900

    feature : 이메일 초대를 통한 캠페인 가입 시 이미 가입되어 있는지, 이미 친구인지 확인하는 로직 추가

--- src/main/java/com/keypoint/keypointtravel/campaign/repository/MemberCampaignRepository.java
+++ src/main/java/com/keypoint/keypointtravel/campaign/repository/MemberCampaignRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberCampaignRepository extends JpaRepository<MemberCampaign, Long> {
 
     List<MemberCampaign> findAllByCampaignId(Long campaignId);
+
+    boolean existsByCampaignIdAndMemberId(Long campaignId, Long memberId);
 }
