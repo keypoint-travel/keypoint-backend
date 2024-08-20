fcd29cca0d5d09682956711c56168f7aeebe8bc8
chea-young <lchy0413@gmail.com>
2024-08-20 14:20:56 +0900

    [Feature] add entityt, repository, service about receipt (#80)

---
+++ src/main/java/com/keypoint/keypointtravel/receipt/controller/ReceiptController.java
@@ -0,0 +1,13 @@
+package com.keypoint.keypointtravel.receipt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/receipts")
+public class ReceiptController {
+    
+}
