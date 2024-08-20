3696a299a6d1f50241fca14bf8f4aba4f4873ebd
khdscor <khds_cor@naver.com>
2024-08-20 20:58:41 +0900

    refactor: 가입 완료 후 redis 내 데이터 제거 및 코드 리펙토링

--- src/main/java/com/keypoint/keypointtravel/campaign/service/JoinCampaignService.java
+++ src/main/java/com/keypoint/keypointtravel/campaign/service/JoinCampaignService.java
@@ -42,31 +42,52 @@ public class JoinCampaignService {
     @Transactional
     public void joinByEmail(JoinByEmailUseCase useCase) {
         // 이미 캠페인 가입이 된 상태일 시 예외 처리
-        if (memberCampaignRepository.existsByCampaignIdAndMemberId(useCase.getCampaignId(), useCase.getMemberId())) {
-            throw new GeneralException(CampaignErrorCode.DUPLICATED_MEMBER);
-        }
+        validateJoinedCampaign(useCase.getCampaignId(), useCase.getMemberId());
         // redis 캠페인 초대 이메일 목록에 저장되어 있는지(만료되지 않았는지 확인)
-        List<EmailInvitationHistory> histories =
-            emailInvitationHistoryRepository.findByCampaignId(useCase.getCampaignId());
-        boolean isEmailExists = histories.stream()
-            .anyMatch(history -> history.getEmail().equals(useCase.getEmail()));
-        if (!isEmailExists) {
-            throw new GeneralException(CampaignErrorCode.EXPIRED_INVITE_EMAIL);
-        }
+        List<EmailInvitationHistory> emailHistories = validateEmailHistoryInRedis(useCase);
         // 캠페인 리더 member 추출 & 진행 중인 캠페인인지 확인
         MemberCampaign leader = campaignRepository.findCampaignLeader(useCase.getCampaignId());
         // 회원 - 캠페인 태이블 추가(가입)
+        Member member = saveMemberCampaign(useCase);
+        // 캠페인 리더와 친구관계 구축
+        saveFriends(leader, member);
+        // redis 에서 해당 이메일 초대 이력 삭제(중복 포함)
+        emailInvitationHistoryRepository.deleteAll(emailHistories);
+        // todo : 대상자 및 캠페인 참여 인원들에게 알림 발송
+    }
+
+    private void validateJoinedCampaign(Long campaignId, Long memberId) {
+        if (memberCampaignRepository.existsByCampaignIdAndMemberId(campaignId, memberId)) {
+            throw new GeneralException(CampaignErrorCode.DUPLICATED_MEMBER);
+        }
+    }
+
+    private List<EmailInvitationHistory> validateEmailHistoryInRedis(JoinByEmailUseCase useCase) {
+        List<EmailInvitationHistory> histories =
+            emailInvitationHistoryRepository.findByCampaignId(useCase.getCampaignId());
+        // 중복 포함 동일한 email 가진 기록 조회
+        List<EmailInvitationHistory> emailHistories = histories.stream()
+            .filter(history -> history.getEmail().equals(useCase.getEmail()))
+            .toList();
+        // 기간 만료 시 예외 처리
+        if (emailHistories.isEmpty()) {
+            throw new GeneralException(CampaignErrorCode.EXPIRED_INVITE_EMAIL);
+        }
+        return histories;
+    }
+
+    private Member saveMemberCampaign(JoinByEmailUseCase useCase) {
         Member member = memberRepository.getReferenceById(useCase.getMemberId());
         Campaign campaign = campaignRepository.getReferenceById(useCase.getCampaignId());
         memberCampaignRepository.save(new MemberCampaign(campaign, member, false));
+        return member;
+    }
 
-        // 캠페인 리더와 친구관계 구축
+    private void saveFriends(MemberCampaign leader, Member member) {
         if (!friendRepository.existsByFriendIdAndMemberIdAndIsDeletedFalse(leader.getMember().getId(), member.getId())) {
             friendRepository.save(buildFriend(leader.getMember(), member));
             friendRepository.save(buildFriend(member, leader.getMember()));
         }
-        // todo: redis 에서 삭제 로직
-        // todo : 대상자 및 캠페인 참여 인원들에게 알림 발송
     }
 
     private Friend buildFriend(Member findedMember, Member member) {
