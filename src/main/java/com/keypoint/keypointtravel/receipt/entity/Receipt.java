5b7823cf932b8f896430d3c09b9adac3cbb6e673
chea-young <lchy0413@gmail.com>
2024-08-20 17:19:29 +0900

    [Feature] update dto about receipt (#80)

--- src/main/java/com/keypoint/keypointtravel/receipt/entity/Receipt.java
+++ src/main/java/com/keypoint/keypointtravel/receipt/entity/Receipt.java
@@ -1,11 +1,13 @@
 package com.keypoint.keypointtravel.receipt.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.annotations.Comment;
 import org.joda.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 
+import com.keypoint.keypointtravel.campaign.entity.Campaign;
 import com.keypoint.keypointtravel.global.entity.BaseEntity;
 import com.keypoint.keypointtravel.global.enumType.receipt.ReceiptCategory;
 import com.keypoint.keypointtravel.global.enumType.receipt.ReceiptRegistrationType;
@@ -14,8 +16,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -33,10 +38,15 @@ public class Receipt extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "campaign_id")
+    private Campaign campaign;
+
     @OneToMany(mappedBy = "payment_item", orphanRemoval = true)
     private List<PaymentItem> paymentItems = new ArrayList<>();
 
     @Comment("매장 이름")
+    @Column(nullable = false)
     private String store;
 
     @Comment("매장 주소")
@@ -51,8 +61,10 @@ public class Receipt extends BaseEntity {
 
     private String memo;
 
+    @Column(nullable = false)
     private LocalDateTime paidAt;
 
+    @Column(nullable = false)
     private Long receiptImageId;
 
     @Comment("경도")
