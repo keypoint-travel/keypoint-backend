a03f24f36c0691d28cf777dcec9f9a73bcaac46b
khdscor <khds_cor@naver.com>
2024-08-21 21:08:00 +0900

    feature : 캠페인 코드를 통한 캠페인 참여 신청 api 구현

--- src/main/java/com/keypoint/keypointtravel/campaign/controller/JoinCampaignController.java
+++ src/main/java/com/keypoint/keypointtravel/campaign/controller/JoinCampaignController.java
@@ -1,32 +1,42 @@
 package com.keypoint.keypointtravel.campaign.controller;
 
+import com.keypoint.keypointtravel.campaign.dto.request.JoinByCampaignCodeRequest;
+import com.keypoint.keypointtravel.campaign.dto.useCase.JoinByCodeUseCase;
 import com.keypoint.keypointtravel.campaign.dto.useCase.JoinByEmailUseCase;
 import com.keypoint.keypointtravel.campaign.service.JoinCampaignService;
 import com.keypoint.keypointtravel.global.config.security.CustomUserDetails;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/campaigns/{campaignId}/participation")
+@RequestMapping("/api/v1/campaigns")
 @RequiredArgsConstructor
 public class JoinCampaignController {
 
     private final JoinCampaignService joinCampaignService;
 
-    @PostMapping("/email")
+    @PostMapping("/{campaignId}/participation/email")
     @PreAuthorize("hasRole('ROLE_CERTIFIED_USER')")
     public ResponseEntity<Void> joinByEmail(
         @PathVariable("campaignId") Long campaignId,
         @AuthenticationPrincipal CustomUserDetails userDetails) {
         joinCampaignService.joinByEmail(
             new JoinByEmailUseCase(userDetails.getId(), userDetails.getEmail(), campaignId));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/participation/code")
+    @PreAuthorize("hasRole('ROLE_CERTIFIED_USER')")
+    public ResponseEntity<Void> requestJoinByCampaignCode(
+        @RequestBody @Valid JoinByCampaignCodeRequest request,
+        @AuthenticationPrincipal CustomUserDetails userDetails) {
+        joinCampaignService.requestJoinByCampaignCode(
+            new JoinByCodeUseCase(userDetails.getId(), request.getCampaignCode()));
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
