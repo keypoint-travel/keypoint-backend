7506092aed73bb6aaeb47c5d2e0a03e725ea5e97
khdscor <khds_cor@naver.com>
2024-08-22 21:31:32 +0900

    refactor : 캠페인 카테고리 별 결제 내역 조회 비즈니스 로직 리펙토링

--- src/main/java/com/keypoint/keypointtravel/campaign/service/ReadCampaignService.java
+++ src/main/java/com/keypoint/keypointtravel/campaign/service/ReadCampaignService.java
@@ -39,49 +39,88 @@ public class ReadCampaignService {
      * 캠페인 카테고리 별 결제 내역 조회 함수
      *
      * @Param campaignId, currencyType, memberId useCase
-     * @Return
+     * @Return DetailsByCategoryResponse
      */
     @Transactional
     public DetailsByCategoryResponse findByCategory(FindPaymentUseCase useCase) {
         // 0. 캠페인에 소속되어 있는지 검증
-        if (!memberCampaignRepository.existsByCampaignIdAndMemberId(useCase.getCampaignId(), useCase.getMemberId())) {
-            throw new GeneralException(CampaignErrorCode.NOT_EXISTED_CAMPAIGN);
-        }
-        // 1. 캠페인 아이디를 통해 총 에산 조회 TotalBudgetDto
+        validateInCampaign(useCase);
+        // 1. 캠페인 아이디를 통해 총 에산 조회
         TotalBudgetDto totalBudget = campaignBudgetRepository.findTotalBudgetByCampaignId(useCase.getCampaignId());
         // 2. 캠페인 아이디를 통해 결제 항목 리스트 조회
         List<PaymentDto> paymentDtoList = customPaymentRepository.findPaymentList(useCase.getCampaignId());
         // 3. 결제 항목 별 참여 인원 리스트 조회
         List<Long> paymentItemIds = paymentDtoList.stream().map(PaymentDto::getPaymentItemId).toList();
         List<PaymentMemberDto> paymentMemberDtoList = customPaymentRepository.findMemberListByPayments(paymentItemIds);
-
-        List<Currency> currencies = currencyRepository.findAll();
         // 4. 화폐 타입을 따로 지정할 경우 : totalBudget, paymentDtoList 의 화폐 타입과 금액을 변환
         if (useCase.getCurrencyType() != null) {
-            totalBudget.updateTotalBudget(
-                convertCurrency(currencies, totalBudget.getTotalAmount(), totalBudget.getCurrencyType(), useCase.getCurrencyType()),
-                useCase.getCurrencyType()
-            );
-            paymentDtoList.forEach(paymentDto -> paymentDto.updateBudget(
-                convertCurrency(currencies, paymentDto.getAmount(), paymentDto.getCurrencyType(), useCase.getCurrencyType()),
-                useCase.getCurrencyType()
-            ));
+            List<Currency> currencies = currencyRepository.findAll();
+            updateCurrency(useCase, totalBudget, paymentDtoList, currencies);
         }
         // 5. 카테고리 별 금액 계산 및 잔여 예산 계산
+        HashMap<String, Float> categoryAmount = calculateCategoryAmount(paymentDtoList);
+        float totalAmount = categoryAmount.values().stream().reduce(0f, Float::sum);
+        float remainBudget = calculateRemainBudget(categoryAmount, totalBudget, totalAmount);
+        totalAmount += remainBudget;
+        // 6. 카테고리 별 비율 계산
+        HashMap<String, Float> categoryPercentage = calculateCategoryPercentage(categoryAmount, totalAmount);
+        // 7. 결제 항목 응답값 생성 및 응답
+        return createResponse(totalBudget, totalAmount, remainBudget, categoryPercentage, paymentDtoList, paymentMemberDtoList);
+    }
+
+    // 캠페인에 소속되어 있는지 검증
+    private void validateInCampaign(FindPaymentUseCase useCase) {
+        if (!memberCampaignRepository.existsByCampaignIdAndMemberId(useCase.getCampaignId(), useCase.getMemberId())) {
+            throw new GeneralException(CampaignErrorCode.NOT_EXISTED_CAMPAIGN);
+        }
+    }
+
+    // totalBudget, paymentDtoList 의 화폐 타입과 금액을 변환
+    private void updateCurrency(FindPaymentUseCase useCase, TotalBudgetDto totalBudget,
+                                List<PaymentDto> paymentDtoList, List<Currency> currencies) {
+        totalBudget.updateTotalBudget(
+            convertCurrency(currencies, totalBudget.getTotalAmount(), totalBudget.getCurrencyType(), useCase.getCurrencyType()),
+            useCase.getCurrencyType()
+        );
+        paymentDtoList.forEach(paymentDto -> paymentDto.updateBudget(
+            convertCurrency(currencies, paymentDto.getAmount(), paymentDto.getCurrencyType(), useCase.getCurrencyType()),
+            useCase.getCurrencyType()
+        ));
+    }
+
+    private float convertCurrency(List<Currency> currencies, float amount, CurrencyType from, CurrencyType to) {
+        Currency fromCurrency = currencies.stream()
+            .filter(currency -> currency.getCur_unit().equals(from.getCode()))
+            .findFirst().orElseThrow(() -> new GeneralException(CampaignErrorCode.NOT_EXISTED_CURRENCY));
+        Currency toCurrency = currencies.stream()
+            .filter(currency -> currency.getCur_unit().equals(to.getCode()))
+            .findFirst().orElseThrow(() -> new GeneralException(CampaignErrorCode.NOT_EXISTED_CURRENCY));
+        return (float) (amount * fromCurrency.getExchange_rate() / toCurrency.getExchange_rate());
+    }
+
+    // 카테고리 별 금액 계산
+    private HashMap<String, Float> calculateCategoryAmount(List<PaymentDto> paymentDtoList) {
         HashMap<String, Float> categoryAmount = new HashMap<>();
         paymentDtoList.forEach(paymentDto -> {
             String category = paymentDto.getCategory().getDescription();
             categoryAmount.put(category, categoryAmount.getOrDefault(
                 category, 0f) + paymentDto.getAmount() * paymentDto.getQuantity());
         });
-        float totalAmount = categoryAmount.values().stream().reduce(0f, Float::sum);
+        return categoryAmount;
+    }
+
+    // 잔여 예산 계산
+    private float calculateRemainBudget(HashMap<String, Float> categoryAmount, TotalBudgetDto totalBudget, float totalAmount) {
         float remainBudget = 0;
         if (totalBudget.getTotalAmount() - totalAmount > 0) {
             remainBudget = totalBudget.getTotalAmount() - totalAmount;
             categoryAmount.put("잔여 예산", remainBudget);
-            totalAmount += remainBudget;
         }
-        // 6. 카테고리 별 비율 계산
+        return remainBudget;
+    }
+
+    // 카테고리 별 비율 계산
+    private HashMap<String, Float> calculateCategoryPercentage(HashMap<String, Float> categoryAmount, float totalAmount) {
         HashMap<String, Float> categoryPercentage = new HashMap<>();
         // 최대 비율을 가지는 카테고리 선정
         String maxCategory = null;
@@ -101,12 +140,17 @@ public class ReadCampaignService {
         if (maxCategory != null) {
             categoryPercentage.put(maxCategory, maxPercentage + diff);
         }
-        // 7. 결제 항목 응답값 생성
+        return categoryPercentage;
+    }
+
+    // 카테고리 별 결제 항목 응답값 생성 및 응답
+    private DetailsByCategoryResponse createResponse(TotalBudgetDto totalBudget, float totalAmount, float remainBudget,
+                                                     HashMap<String, Float> categoryPercentage, List<PaymentDto> paymentDtoList,
+                                                     List<PaymentMemberDto> paymentMemberDtoList) {
         List<PaymentInfo> paymentInfoList = paymentDtoList.stream()
             .map(PaymentInfo::from)
             .toList();
         paymentInfoList.forEach(paymentInfo -> paymentInfo.addMembers(paymentMemberDtoList));
-        // 8. 응답
         return new DetailsByCategoryResponse(
             totalBudget.getCurrencyType().getCode(),
             totalAmount,
@@ -114,16 +158,5 @@ public class ReadCampaignService {
             categoryPercentage,
             paymentInfoList
         );
-    }
-
-    private float convertCurrency(List<Currency> currencies, float amount, CurrencyType from, CurrencyType to) {
-
-        Currency fromCurrency = currencies.stream()
-            .filter(currency -> currency.getCur_unit().equals(from.getCode()))
-            .findFirst().orElseThrow(() -> new GeneralException(CampaignErrorCode.NOT_EXISTED_CURRENCY));
-        Currency toCurrency = currencies.stream()
-            .filter(currency -> currency.getCur_unit().equals(to.getCode()))
-            .findFirst().orElseThrow(() -> new GeneralException(CampaignErrorCode.NOT_EXISTED_CURRENCY));
-        return (float) (amount * fromCurrency.getExchange_rate() / toCurrency.getExchange_rate());
     }
 }
