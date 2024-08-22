5ab5486c91f58b068dd51fc2ddc0932d631588c6
chea-young <lchy0413@gmail.com>
2024-08-23 00:31:42 +0900

    [Fix] Modify detail data not to be null transformed (#39)

--- src/main/java/com/keypoint/keypointtravel/notification/event/pushNotification/CampaignPushNotificationEvent.java
+++ src/main/java/com/keypoint/keypointtravel/notification/event/pushNotification/CampaignPushNotificationEvent.java
@@ -1,13 +1,15 @@
 package com.keypoint.keypointtravel.notification.event.pushNotification;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.keypoint.keypointtravel.global.enumType.notification.PushNotificationType;
 import java.util.List;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CampaignPushNotificationEvent extends PushNotificationEvent {
 
-    private Long campaignId;
+    private CampaignData additionalData;
 
     public CampaignPushNotificationEvent() {
         super(null, null);
@@ -19,7 +21,7 @@ public class CampaignPushNotificationEvent extends PushNotificationEvent {
         Long campaignId
     ) {
         super(type, memberIds);
-        this.campaignId = campaignId;
+        this.additionalData = new CampaignData(campaignId);
     }
 
     public static CampaignPushNotificationEvent of(
@@ -31,7 +33,27 @@ public class CampaignPushNotificationEvent extends PushNotificationEvent {
     }
 
     @Override
-    public Long getAdditionalData() {
-        return this.campaignId;
+    public CampaignData getAdditionalData() {
+        return additionalData;
+    }
+
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class CampaignData {
+
+        @JsonProperty("campaignId") // JSON 매핑을 명시적으로 설정
+        private Long campaignId;
+
+        public CampaignData(Long campaignId) {
+            this.campaignId = campaignId;
+        }
+
+        public Long getCampaignId() {
+            return campaignId;
+        }
+
+        public void setCampaignId(Long campaignId) { // Setter 추가 (필요 시)
+            this.campaignId = campaignId;
+        }
     }
 }
