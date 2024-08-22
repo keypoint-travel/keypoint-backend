fd5fa2c420b6d84a4e3f3ab8440ece46ce858eff
chea-young <lchy0413@gmail.com>
2024-08-22 19:21:00 +0900

    [Feature] Add language about fcm (#93)

--- src/main/java/com/keypoint/keypointtravel/global/enumType/notification/PushNotificationType.java
+++ src/main/java/com/keypoint/keypointtravel/global/enumType/notification/PushNotificationType.java
@@ -8,9 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum PushNotificationType {
     RECEIPT_REGISTER("영수증 등록"),
     FRIEND_INVITE("친구 초대 알림"),
+    CAMPAIGN_D_DAY("캠페인 D-1 알림"),
     CAMPAIGN_START("캠페인 시작 알림"),
     CAMPAIGN_END("캠페인 종료 알림"),
-    CAMPAIGN_D_DAY("캠페인 D-1 알림"),
     CAMPAIGN_INVITE("캠페인 초대 알림"),
     CAMPAIGN_REGISTRATION("캠페인 등록 알림"),
     EVENT_NOTICE("이벤트/전체 공지 알림"),
