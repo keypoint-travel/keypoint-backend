837b76db18cc2950c779293a8b03aa467f521f4f
chea-young <63410509+chea-young@users.noreply.github.com>
2024-08-23 11:39:58 +0900

    Revert "[Fix] Fix issue that fcm multilingual does not apply properly (#39)"
    This reverts commit f8502ed4ea0a5ad5c9b131dc959fc0b593ed1144.

--- src/main/java/com/keypoint/keypointtravel/notification/service/FCMService.java
+++ src/main/java/com/keypoint/keypointtravel/notification/service/FCMService.java
@@ -2,11 +2,8 @@ package com.keypoint.keypointtravel.notification.service;
 
 import com.keypoint.keypointtravel.global.enumType.notification.PushNotificationType;
 import com.keypoint.keypointtravel.notification.event.pushNotification.CampaignAcceptorPushNotificationEvent;
-import com.keypoint.keypointtravel.notification.event.pushNotification.CampaignApplicantPushNotificationEvent;
 import com.keypoint.keypointtravel.notification.event.pushNotification.CampaignLeaderPushNotificationEvent;
 import com.keypoint.keypointtravel.notification.event.pushNotification.CampaignPushNotificationEvent;
-import com.keypoint.keypointtravel.notification.event.pushNotification.CommonPushNotificationEvent;
-import com.keypoint.keypointtravel.notification.event.pushNotification.FriendPushNotificationEvent;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
@@ -21,36 +18,36 @@ public class FCMService {
     private final ApplicationEventPublisher eventPublisher;
 
     public void testEvent() {
-        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
-            PushNotificationType.RECEIPT_REGISTER,
-            List.of(5L)
-        ));
-
-        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
-            PushNotificationType.CAMPAIGN_D_MINUS_7,
-            List.of(5L)
-        ));
-        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
-            PushNotificationType.CAMPAIGN_D_DAY,
-            List.of(5L)
-        ));
-        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
-            PushNotificationType.CAMPAIGN_NO_EXPENSE_D1,
-            List.of(5L)
-        ));
-        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
-            PushNotificationType.PAYMENT_COMPLETION,
-            List.of(5L)
-        ));
-        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
-            PushNotificationType.INQUIRY_RESPONSE_COMPLETED,
-            List.of(5L)
-        ));
-        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
-            PushNotificationType.CAMPAIGN_D60_PASSED,
-            List.of(5L)
-        ));
-
+//        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
+//            PushNotificationType.RECEIPT_REGISTER,
+//            List.of(5L)
+//        ));
+//
+//        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
+//            PushNotificationType.CAMPAIGN_D_MINUS_7,
+//            List.of(5L)
+//        ));
+//        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
+//            PushNotificationType.CAMPAIGN_D_DAY,
+//            List.of(5L)
+//        ));
+//        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
+//            PushNotificationType.CAMPAIGN_NO_EXPENSE_D1,
+//            List.of(5L)
+//        ));
+//        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
+//            PushNotificationType.PAYMENT_COMPLETION,
+//            List.of(5L)
+//        ));
+//        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
+//            PushNotificationType.INQUIRY_RESPONSE_COMPLETED,
+//            List.of(5L)
+//        ));
+//        eventPublisher.publishEvent(CommonPushNotificationEvent.of(
+//            PushNotificationType.CAMPAIGN_D60_PASSED,
+//            List.of(5L)
+//        ));
+//
         eventPublisher.publishEvent(CampaignLeaderPushNotificationEvent.of(
             PushNotificationType.CAMPAIGN_INVITE,
             List.of(5L),
@@ -76,28 +73,28 @@ public class FCMService {
             List.of(5L),
             1L
         ));
-
-        eventPublisher.publishEvent(CampaignApplicantPushNotificationEvent.of(
-            PushNotificationType.CAMPAIGN_JOIN_REQUEST,
-            List.of(5L),
-            "신청자",
-            1L
-        ));
-
-        eventPublisher.publishEvent(FriendPushNotificationEvent.of(
-            PushNotificationType.FRIEND_ADDED,
-            List.of(5L),
-            "도레"
-        ));
-        eventPublisher.publishEvent(FriendPushNotificationEvent.of(
-            PushNotificationType.FRIEND_ACCEPTED_RECEIVER,
-            List.of(5L),
-            "미파"
-        ));
-        eventPublisher.publishEvent(FriendPushNotificationEvent.of(
-            PushNotificationType.FRIEND_ACCEPTED_SENDER,
-            List.of(5L),
-            "솔라"
-        ));
+//
+//        eventPublisher.publishEvent(CampaignApplicantPushNotificationEvent.of(
+//            PushNotificationType.CAMPAIGN_JOIN_REQUEST,
+//            List.of(5L),
+//            "신청자",
+//            1L
+//        ));
+//
+//        eventPublisher.publishEvent(FriendPushNotificationEvent.of(
+//            PushNotificationType.FRIEND_ADDED,
+//            List.of(5L),
+//            "도레"
+//        ));
+//        eventPublisher.publishEvent(FriendPushNotificationEvent.of(
+//            PushNotificationType.FRIEND_ACCEPTED_RECEIVER,
+//            List.of(5L),
+//            "미파"
+//        ));
+//        eventPublisher.publishEvent(FriendPushNotificationEvent.of(
+//            PushNotificationType.FRIEND_ACCEPTED_SENDER,
+//            List.of(5L),
+//            "솔라"
+//        ));
     }
 }
