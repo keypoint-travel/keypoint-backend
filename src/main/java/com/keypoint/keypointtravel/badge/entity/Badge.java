ddf5b74fac89f59db47b879e07c1dc06b2c4bcf1
chea-young <lchy0413@gmail.com>
2024-08-24 10:46:55 +0900

    [Feature] Add entity about badge [Issue no] #98

---
+++ src/main/java/com/keypoint/keypointtravel/badge/entity/Badge.java
@@ -0,0 +1,38 @@
+package com.keypoint.keypointtravel.badge.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "badge")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Badge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "badge_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Long activeImageId;
+
+    @Column(nullable = false)
+    private Long inactiveImageId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, name = "is_deleted")
+    private boolean isDeleted;
+
+    @Column(nullable = false, unique = true, name = "order_number")
+    private int order;
+}
