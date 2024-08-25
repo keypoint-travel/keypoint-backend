582b76665fe5c9f5d3ce6a7b10aa7c73f5f80324
chea-young <lchy0413@gmail.com>
2024-08-25 20:20:43 +0900

    [Feature] Add DELETE /api/v1/badges

--- src/main/java/com/keypoint/keypointtravel/badge/controller/AdminBadgeController.java
+++ src/main/java/com/keypoint/keypointtravel/badge/controller/AdminBadgeController.java
@@ -2,15 +2,18 @@ package com.keypoint.keypointtravel.badge.controller;
 
 import com.keypoint.keypointtravel.badge.dto.request.CreateBadgeRequest;
 import com.keypoint.keypointtravel.badge.dto.useCase.CreateBadgeUseCase;
+import com.keypoint.keypointtravel.badge.dto.useCase.DeleteBadgeUseCase;
 import com.keypoint.keypointtravel.badge.dto.useCase.UpdateBadgeUseCase;
 import com.keypoint.keypointtravel.badge.service.AdminBadgeService;
 import com.keypoint.keypointtravel.global.dto.response.APIResponseEntity;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -57,6 +60,20 @@ public class AdminBadgeController {
 
         return APIResponseEntity.<Void>builder()
             .message("배지 수정 성공")
+            .build();
+    }
+
+    @DeleteMapping()
+    public APIResponseEntity<Void> deleteBadge(
+        @RequestParam("badge-ids") Long[] badgeIds
+    ) {
+        DeleteBadgeUseCase useCase = DeleteBadgeUseCase.from(
+            badgeIds
+        );
+        adminBadgeService.deleteBadge(useCase);
+
+        return APIResponseEntity.<Void>builder()
+            .message("배지 삭제 성공")
             .build();
     }
 }
--- src/main/java/com/keypoint/keypointtravel/badge/respository/AdminBadgeCustomRepository.java
+++ src/main/java/com/keypoint/keypointtravel/badge/respository/AdminBadgeCustomRepository.java
@@ -1,8 +1,11 @@
 package com.keypoint.keypointtravel.badge.respository;
 
+import com.keypoint.keypointtravel.badge.dto.useCase.DeleteBadgeUseCase;
 import com.keypoint.keypointtravel.badge.dto.useCase.UpdateBadgeUseCase;
 
 public interface AdminBadgeCustomRepository {
 
     void updateBadge(UpdateBadgeUseCase useCase);
+
+    void deleteGuides(DeleteBadgeUseCase useCase);
 }
