e19620b68086bee5e075cdb1f66e308970c6f0b6
chea-young <lchy0413@gmail.com>
2024-08-27 18:51:08 +0900

    [Feature] Modify GET /api/v1/guides/management [Issue no] #114

--- src/main/java/com/keypoint/keypointtravel/guide/service/ReadGuideService.java
+++ src/main/java/com/keypoint/keypointtravel/guide/service/ReadGuideService.java
@@ -57,7 +57,7 @@ public class ReadGuideService {
      */
     public Page<ReadGuideInAdminResponse> findGuidesInAdmin(PageUseCase useCase) {
         try {
-            return guideRepository.findGuidesInAdmin(useCase.getPageable());
+            return guideRepository.findGuidesInAdmin(useCase);
         } catch (Exception ex) {
             throw new GeneralException(ex);
         }
