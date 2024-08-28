2ac62a238303f4e34e8b960022a07dda94bbfa31
chea-young <lchy0413@gmail.com>
2024-08-29 00:44:32 +0900

    [Feature] Change Get to Post

--- src/main/java/com/keypoint/keypointtravel/notification/controller/FCMController.java
+++ src/main/java/com/keypoint/keypointtravel/notification/controller/FCMController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +29,7 @@ public class FCMController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/send")
+    @PostMapping("/send")
     public APIResponseEntity<Void> sendFCM(
         @RequestBody FCMTestRequest request) {
 
