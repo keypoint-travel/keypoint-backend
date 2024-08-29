d55833506943e2a10e3306bb5bf27c172e3f05f4
chea-young <lchy0413@gmail.com>
2024-08-29 21:33:57 +0900

    [Feature] Modify POST /api/v1/guides  - 썸네일과 순서만 수정 가능하도록 수정

--- src/main/java/com/keypoint/keypointtravel/guide/controller/UpdateGuideController.java
+++ src/main/java/com/keypoint/keypointtravel/guide/controller/UpdateGuideController.java
@@ -2,12 +2,15 @@ package com.keypoint.keypointtravel.guide.controller;
 
 import com.keypoint.keypointtravel.global.dto.response.APIResponseEntity;
 import com.keypoint.keypointtravel.guide.dto.request.updateGuide.UpdateGuideRequest;
+import com.keypoint.keypointtravel.guide.dto.request.updateGuide.UpdateGuideTranslationRequest;
+import com.keypoint.keypointtravel.guide.dto.useCase.updateGuide.UpdateGuideTranslationUseCase;
 import com.keypoint.keypointtravel.guide.dto.useCase.updateGuide.UpdateGuideUseCase;
 import com.keypoint.keypointtravel.guide.service.UpdateGuideService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,21 +37,21 @@ public class UpdateGuideController {
             .build();
     }
 
-//    @DeleteMapping("/{guideId}/translations/{guideTranslationId}")
-//    public APIResponseEntity<Void> updateGuideTranslation(
-//        @PathVariable(value = "guideId") Long guideId,
-//        @PathVariable(value = "guideTranslationId") Long guideTranslationId,
-//        @Valid @RequestBody() UpdateGuideTranslationRequest request
-//    ) {
-////        UpdateGuideTranslationUseCase useCase = UpdateGuideTranslationUseCase.of(
-////            guideId,
-////            guideTranslationId,
-////            request
-////        );
-////        updateGuideService.updateGuide(useCase);
-//
-//        return APIResponseEntity.<Void>builder()
-//            .message("특정 이용 가이드 번역 정보 수정 성공")
-//            .build();
-//    }
+    @PutMapping("/{guideId}/translations/{guideTranslationId}")
+    public APIResponseEntity<Void> updateGuideTranslation(
+        @PathVariable(value = "guideId") Long guideId,
+        @PathVariable(value = "guideTranslationId") Long guideTranslationId,
+        @Valid @RequestBody UpdateGuideTranslationRequest request
+    ) {
+        UpdateGuideTranslationUseCase useCase = UpdateGuideTranslationUseCase.of(
+            guideId,
+            guideTranslationId,
+            request
+        );
+        updateGuideService.updateGuideTranslation(useCase);
+
+        return APIResponseEntity.<Void>builder()
+            .message("특정 이용 가이드 번역 정보 수정 성공")
+            .build();
+    }
 }
--- src/main/java/com/keypoint/keypointtravel/guide/dto/request/updateGuide/UpdateGuideTranslationRequest.java
+++ src/main/java/com/keypoint/keypointtravel/guide/dto/request/updateGuide/UpdateGuideTranslationRequest.java
@@ -1,14 +1,10 @@
 package com.keypoint.keypointtravel.guide.dto.request.updateGuide;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class UpdateGuideTranslationRequest {
-
-  @NotNull
-  private Long guideTranslationId;
 
   @NotBlank
   private String title;
