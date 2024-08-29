ebc0ba208ba05b627b95fde3308e37fc6f9b763e
chea-young <lchy0413@gmail.com>
2024-08-29 22:55:48 +0900

    [Feature] Add token information in response of updateMemberProfile

--- src/main/java/com/keypoint/keypointtravel/member/service/CreateMemberService.java
+++ src/main/java/com/keypoint/keypointtravel/member/service/CreateMemberService.java
@@ -1,5 +1,7 @@
 package com.keypoint.keypointtravel.member.service;
 
+import com.keypoint.keypointtravel.auth.dto.response.TokenInfoResponse;
+import com.keypoint.keypointtravel.auth.service.AuthService;
 import com.keypoint.keypointtravel.global.enumType.email.EmailTemplate;
 import com.keypoint.keypointtravel.global.enumType.error.MemberErrorCode;
 import com.keypoint.keypointtravel.global.enumType.member.OauthProviderType;
@@ -36,6 +38,7 @@ public class CreateMemberService {
 
     private static final int EMAIL_VERIFICATION_CODE_DIGITS = 6;
 
+    private final AuthService authService;
     private final MemberRepository memberRepository;
     private final MemberConsentRepository memberConsentRepository;
     private final MemberDetailRepository memberDetailRepository;
@@ -78,7 +81,10 @@ public class CreateMemberService {
             //5. 초대코드 저장: 초대코드가 중복되지 않도록 문자열에 마지막에 memberId를 추가하여 저장
             member.setInvitationCode(StringUtils.getRandomString(9) + member.getId());
 
-            return MemberResponse.from(member);
+            // 6. 토큰 발금
+            TokenInfoResponse response = authService.getJwtTokenInfo(email, password);
+
+            return MemberResponse.of(member, response);
         } catch (Exception ex) {
             throw new GeneralException(ex);
         }
