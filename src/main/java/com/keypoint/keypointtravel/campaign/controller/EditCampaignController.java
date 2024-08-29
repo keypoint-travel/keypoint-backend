af8591fc0d21bfbe2da24eb8b628b9f366a6fbde
khdscor <khds_cor@naver.com>
2024-08-29 15:29:21 +0900

    feature: 캠페인 수정 api 구현

--- src/main/java/com/keypoint/keypointtravel/campaign/controller/EditCampaignController.java
+++ src/main/java/com/keypoint/keypointtravel/campaign/controller/EditCampaignController.java
@@ -1,15 +1,20 @@
 package com.keypoint.keypointtravel.campaign.controller;
 
+import com.keypoint.keypointtravel.campaign.dto.request.createRequest.EditRequest;
 import com.keypoint.keypointtravel.campaign.dto.response.EditCampaignResponse;
 import com.keypoint.keypointtravel.campaign.dto.useCase.FIndCampaignUseCase;
+import com.keypoint.keypointtravel.campaign.dto.useCase.UpdateUseCase;
 import com.keypoint.keypointtravel.campaign.service.EditCampaignService;
 import com.keypoint.keypointtravel.global.config.security.CustomUserDetails;
 import com.keypoint.keypointtravel.global.dto.response.APIResponseEntity;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/campaigns/{campaignId}")
@@ -29,5 +34,17 @@ public class EditCampaignController {
             .message("캠페인 조회 성공")
             .data(response)
             .build();
+    }
+
+    @PreAuthorize("hasRole('ROLE_CERTIFIED_USER')")
+    @PutMapping
+    public ResponseEntity<Void> editCampaign(
+        @PathVariable Long campaignId,
+        @RequestPart(value = "coverImage", required = false) MultipartFile coverImage,
+        @RequestPart(value = "detail") @Valid EditRequest request,
+        @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        editCampaignService.editCampaign(UpdateUseCase.of(campaignId, coverImage, request, userDetails.getId()));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
