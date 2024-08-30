4d0e6a3202704361439c57adac339a4503bd5c46
chea-young <lchy0413@gmail.com>
2024-08-30 23:56:59 +0900

    [Feature] Add type (#129) - 배지를 구분하기 위한 type 추가

--- src/main/java/com/keypoint/keypointtravel/badge/entity/Badge.java
+++ src/main/java/com/keypoint/keypointtravel/badge/entity/Badge.java
@@ -1,8 +1,11 @@
 package com.keypoint.keypointtravel.badge.entity;
 
 import com.keypoint.keypointtravel.global.entity.BaseEntity;
+import com.keypoint.keypointtravel.global.enumType.setting.BadgeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +13,7 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
@@ -36,6 +40,11 @@ public class Badge extends BaseEntity {
 
     @Column(nullable = false, name = "order_number")
     private int order;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("'SIGN_UP'")
+    private BadgeType type;
 
     public Badge(
         Long activeImageId,
---
+++ src/main/java/com/keypoint/keypointtravel/global/enumType/setting/BadgeType.java
@@ -0,0 +1,15 @@
+package com.keypoint.keypointtravel.global.enumType.setting;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum BadgeType {
+    SIGN_UP("SIGN_UP", "회원가입 배지"),
+    FIRST_CAMPAIGN("FIRST_CAMPAIGN", "첫 캠페인 완료 배지"),
+    FRIEND_REGISTER("FRIEND_REGISTER", "친구 등록 배지");
+
+    private final String translatedKey;
+    private final String description;
+}
