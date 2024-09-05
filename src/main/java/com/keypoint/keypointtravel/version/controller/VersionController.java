e329c2fed5fbe7e6643df72b6637aa4a44c9f28d
chea-young <lchy0413@gmail.com>
2024-09-05 12:02:41 +0900

    [Feature] Add GET /api/v1/versions/management (#62)

--- src/main/java/com/keypoint/keypointtravel/version/controller/VersionController.java
+++ src/main/java/com/keypoint/keypointtravel/version/controller/VersionController.java
@@ -1,13 +1,28 @@
 package com.keypoint.keypointtravel.version.controller;
 
+import com.keypoint.keypointtravel.global.dto.response.APIResponseEntity;
+import com.keypoint.keypointtravel.version.dto.response.VersionResponse;
+import com.keypoint.keypointtravel.version.service.VersionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/versions")
 public class VersionController {
+    private final VersionService versionService;
 
-    
+    @GetMapping("/management")
+    public APIResponseEntity<List<VersionResponse>> findVersions() {
+        List<VersionResponse> result = versionService.findVersions();
+
+        return APIResponseEntity.<List<VersionResponse>>builder()
+                .message("전체 버전 정보 조회")
+                .data(result)
+                .build();
+    }
 }
