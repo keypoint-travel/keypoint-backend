477d6b8d849b24b6c4af5885183f36aed76bccbd
chea-young <lchy0413@gmail.com>
2024-09-05 11:43:04 +0900

    [Feature] Add VersionInitializer (#62) - DB에 저장되지 않는 Version type이 존재하는 경우, 생성하도록 수정 (새로운 버전 타입이 생성될 때마다 Enum에만 추가하면 추가 될 수 있도록 설정)

--- src/main/java/com/keypoint/keypointtravel/version/repository/VersionRepository.java
+++ src/main/java/com/keypoint/keypointtravel/version/repository/VersionRepository.java
@@ -1,9 +1,16 @@
 package com.keypoint.keypointtravel.version.repository;
 
+import com.keypoint.keypointtravel.global.enumType.setting.VersionType;
 import com.keypoint.keypointtravel.version.entity.Version;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface VersionRepository extends JpaRepository<Version, Long> {
+
+    @Query("SELECT v.type FROM Version v")
+    Set<VersionType> findVersionType();
 }
