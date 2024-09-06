fb52ae07fe730cb46a0ca968bcecdec013c123b1
chea-young <lchy0413@gmail.com>
2024-09-06 09:26:39 +0900

    [Feature] Add controller, service about visited-country ( #142)

---
+++ src/main/java/com/keypoint/keypointtravel/visitedCountry/controller/VisitedCountryController.java
@@ -0,0 +1,11 @@
+package com.keypoint.keypointtravel.visitedCountry.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/visited-countries")
+public class VisitedCountryController {
+}
