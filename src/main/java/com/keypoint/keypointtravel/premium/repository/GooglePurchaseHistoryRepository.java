6c2e6d18f4bab67dab3adde37f6b6031e63853b6
khdscor <khds_cor@naver.com>
2024-09-06 11:19:47 +0900

    feature : 구글 결제 내역 엔티티 생성

---
+++ src/main/java/com/keypoint/keypointtravel/premium/repository/GooglePurchaseHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.keypoint.keypointtravel.premium.repository;
+
+import com.keypoint.keypointtravel.premium.entity.GooglePurchaseHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GooglePurchaseHistoryRepository extends JpaRepository<GooglePurchaseHistory, Long> {
+}
