6eb935b2946e305ae987a3a511969996e534d76a
chea-young <lchy0413@gmail.com>
2024-09-06 14:17:21 +0900

    [Feature] Add findVisitedCountriesAndCampaigns visited-country ( #142)

--- src/main/java/com/keypoint/keypointtravel/visitedCountry/service/VisitedCountryService.java
+++ src/main/java/com/keypoint/keypointtravel/visitedCountry/service/VisitedCountryService.java
@@ -1,6 +1,14 @@
 package com.keypoint.keypointtravel.visitedCountry.service;
 
+
+import com.keypoint.keypointtravel.global.dto.useCase.PageAndMemberIdUseCase;
+import com.keypoint.keypointtravel.global.enumType.setting.LanguageCode;
+import com.keypoint.keypointtravel.member.repository.memberDetail.MemberDetailRepository;
+import com.keypoint.keypointtravel.visitedCountry.dto.response.searchVisitedCountryResponse.SearchCampaignResponse;
+import com.keypoint.keypointtravel.visitedCountry.dto.response.searchVisitedCountryResponse.SearchVisitedCountryResponse;
+import com.keypoint.keypointtravel.visitedCountry.repository.VisitedCountryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +16,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class VisitedCountryService {
+
+    private final VisitedCountryRepository visitedCountryRepository;
+    private final MemberDetailRepository memberDetailRepository;
+
+    /**
+     * 방문국가 검색 함수
+     *
+     * @param useCase
+     * @return
+     */
+    public SearchVisitedCountryResponse findVisitedCountriesAndCampaigns(PageAndMemberIdUseCase useCase) {
+        LanguageCode languageCode = memberDetailRepository.findLanguageCodeByMemberId(
+                useCase.getMemberId());
+        Page<SearchCampaignResponse> campaigns = visitedCountryRepository.findCampaignsByKeyword(useCase.getMemberId(), languageCode, useCase);
+
+        return null;
+    }
 }
