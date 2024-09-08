f061b6cefca79f22ee25f3ee80ddd8ca2d8e9072
chea-young <lchy0413@gmail.com>
2024-09-08 20:43:33 +0900

    [Feature] Add additional error code. (#93)

--- src/main/java/com/keypoint/keypointtravel/global/enumType/error/BadgeErrorCode.java
+++ src/main/java/com/keypoint/keypointtravel/global/enumType/error/BadgeErrorCode.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum BadgeErrorCode implements ErrorCode {
-    NOT_EXISTED_BADGE("001_NOT_EXISTED_GUIDE", "존재하지 않는 이용 가이드입니다."),
+    NOT_EXISTED_BADGE("001_NOT_EXISTED_BADGE", "존재하지 않는 배지입니다."),
     DUPLICATED_BADGE_NAME("002_DUPLICATED_BADGE_NAME", "이미 존재하는 배지명입니다."),
     BADGE_NOT_OWNED("003_BADGE_NOT_OWNED", "보유한 배지가 아닙니다.");
 
