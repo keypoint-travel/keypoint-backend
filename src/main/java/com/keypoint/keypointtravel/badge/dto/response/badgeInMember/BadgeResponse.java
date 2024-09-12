6fcbd2527cae23f0cc25e5165b18022d7fc241c1
chea-young <lchy0413@gmail.com>
2024-09-12 16:49:16 +0900

    [Feature] Add badgeName in MemberResponse

--- src/main/java/com/keypoint/keypointtravel/badge/dto/response/badgeInMember/BadgeResponse.java
+++ src/main/java/com/keypoint/keypointtravel/badge/dto/response/badgeInMember/BadgeResponse.java
@@ -2,15 +2,10 @@ package com.keypoint.keypointtravel.badge.dto.response.badgeInMember;
 
 import com.keypoint.keypointtravel.badge.entity.Badge;
 import com.keypoint.keypointtravel.badge.entity.EarnedBadge;
-import com.keypoint.keypointtravel.global.enumType.setting.BadgeType;
-import com.keypoint.keypointtravel.global.utils.LogUtils;
 import com.keypoint.keypointtravel.global.utils.MessageSourceUtils;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.context.i18n.LocaleContextHolder;
-
-import java.util.Locale;
 
 @Getter
 @NoArgsConstructor
@@ -31,6 +26,7 @@ public class BadgeResponse {
     ) {
         this.badgeId = badge.getId();
         this.order = badge.getOrder();
+        this.name = MessageSourceUtils.getBadgeName(badge.getType());
 
         if (earnedBadge != null) {
             this.badgeImageUrl = activeImageUrl;
@@ -38,22 +34,6 @@ public class BadgeResponse {
         } else {
             this.badgeImageUrl = inactiveImageUrl;
             this.isEarned = false;
-        }
-
-        setName(badge.getType());
-    }
-
-    private void setName(BadgeType badgeType) {
-        try {
-            Locale currentLocale = LocaleContextHolder.getLocale();
-            String languageKey = badgeType.getLanguageKey();
-            this.name = MessageSourceUtils.getLocalizedLanguage(
-                    languageKey,
-                    currentLocale
-            );
-        } catch (Exception ex) {
-            LogUtils.writeErrorLog("BadgeResponse", "Fail to get language " + ex.getMessage());
-            this.name = badgeType.getDescription();
         }
     }
 }
--- src/main/java/com/keypoint/keypointtravel/member/dto/response/MemberResponse.java
+++ src/main/java/com/keypoint/keypointtravel/member/dto/response/MemberResponse.java
@@ -2,18 +2,32 @@ package com.keypoint.keypointtravel.member.dto.response;
 
 
 import com.keypoint.keypointtravel.auth.dto.response.TokenInfoResponse;
+import com.keypoint.keypointtravel.global.enumType.setting.BadgeType;
+import com.keypoint.keypointtravel.global.utils.MessageSourceUtils;
 import com.keypoint.keypointtravel.member.entity.Member;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class MemberResponse {
 
     private Long id;
     private String email;
     private TokenInfoResponse token;
+    private String name;
     private String badgeUrl;
+
+    public MemberResponse(
+            Long id,
+            String email,
+            TokenInfoResponse token,
+            String badgeUrl
+    ) {
+        this.id = id;
+        this.email = email;
+        this.token = token;
+        this.badgeUrl = badgeUrl;
+        this.name = MessageSourceUtils.getBadgeName(BadgeType.SIGN_UP);
+    }
 
     public static MemberResponse of(Member member, TokenInfoResponse token, String badgeUrl) {
         return new MemberResponse(member.getId(), member.getEmail(), token, badgeUrl);
