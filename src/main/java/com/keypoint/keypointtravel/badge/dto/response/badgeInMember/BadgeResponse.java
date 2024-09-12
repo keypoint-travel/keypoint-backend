175f34a2c855265d93f45571ba3dcb01d26a97cb
chea-young <lchy0413@gmail.com>
2024-09-12 15:07:53 +0900

    [Feature] Apply multilanguage in GET /api/v1/badges

--- src/main/java/com/keypoint/keypointtravel/badge/dto/response/badgeInMember/BadgeResponse.java
+++ src/main/java/com/keypoint/keypointtravel/badge/dto/response/badgeInMember/BadgeResponse.java
@@ -2,9 +2,15 @@ package com.keypoint.keypointtravel.badge.dto.response.badgeInMember;
 
 import com.keypoint.keypointtravel.badge.entity.Badge;
 import com.keypoint.keypointtravel.badge.entity.EarnedBadge;
+import com.keypoint.keypointtravel.global.enumType.setting.BadgeType;
+import com.keypoint.keypointtravel.global.utils.LogUtils;
+import com.keypoint.keypointtravel.global.utils.MessageSourceUtils;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.util.Locale;
 
 @Getter
 @NoArgsConstructor
@@ -24,7 +30,6 @@ public class BadgeResponse {
         String inactiveImageUrl
     ) {
         this.badgeId = badge.getId();
-        this.name = badge.getName();
         this.order = badge.getOrder();
 
         if (earnedBadge != null) {
@@ -35,5 +40,20 @@ public class BadgeResponse {
             this.isEarned = false;
         }
 
+        setName(badge.getType());
+    }
+
+    private void setName(BadgeType badgeType) {
+        try {
+            Locale currentLocale = LocaleContextHolder.getLocale();
+            String languageKey = badgeType.getLanguageKey();
+            this.name = MessageSourceUtils.getLocalizedLanguage(
+                    languageKey,
+                    currentLocale
+            );
+        } catch (Exception ex) {
+            LogUtils.writeErrorLog("BadgeResponse", "Fail to get language " + ex.getMessage());
+            this.name = badgeType.getDescription();
+        }
     }
 }
