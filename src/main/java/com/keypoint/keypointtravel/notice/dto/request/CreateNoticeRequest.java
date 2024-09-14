20a62231ead2cdfcd85d94705284d4356ca30dab
chea-young <lchy0413@gmail.com>
2024-09-15 00:24:50 +0900

    [Feature] Modify POST /api/v1/notices  ( #165)

--- src/main/java/com/keypoint/keypointtravel/notice/dto/request/CreateNoticeRequest.java
+++ src/main/java/com/keypoint/keypointtravel/notice/dto/request/CreateNoticeRequest.java
@@ -2,11 +2,9 @@ package com.keypoint.keypointtravel.notice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class CreateNoticeRequest {
 
     @NotEmpty(message = "제목을 입력해주세요.")
