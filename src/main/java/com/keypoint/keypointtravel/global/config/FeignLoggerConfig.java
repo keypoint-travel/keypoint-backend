ae0c7bf754322a78f4ed1a3001ae541f2fc927f1
chea-young <lchy0413@gmail.com>
2024-09-14 16:39:37 +0900

    [Feature] Update apple key (#48)

---
+++ src/main/java/com/keypoint/keypointtravel/global/config/FeignLoggerConfig.java
@@ -0,0 +1,14 @@
+package com.keypoint.keypointtravel.global.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignLoggerConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
