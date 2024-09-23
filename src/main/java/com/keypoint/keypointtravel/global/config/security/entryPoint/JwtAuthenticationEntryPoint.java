468345f4166e4da6b9240c5182d528f84f7d5f87
chea-young <lchy0413@gmail.com>
2024-09-23 20:57:50 +0900

    [Fix] Disable Authorization Token for permitAll - Authorization 가 붙어있지 않는 API인 경우, 토큰 관련 에러가 나지 않도록 수정

--- src/main/java/com/keypoint/keypointtravel/global/config/security/entryPoint/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/keypoint/keypointtravel/global/config/security/entryPoint/JwtAuthenticationEntryPoint.java
@@ -1,8 +1,14 @@
 package com.keypoint.keypointtravel.global.config.security.entryPoint;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.keypoint.keypointtravel.global.dto.response.ErrorDTO;
+import com.keypoint.keypointtravel.global.enumType.error.ErrorCode;
+import com.keypoint.keypointtravel.global.enumType.error.TokenErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -11,9 +17,28 @@ import org.springframework.stereotype.Component;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request,
+    public void commence(
+        HttpServletRequest request,
         HttpServletResponse response,
-        AuthenticationException authException) throws IOException {
+        AuthenticationException authException
+    ) throws IOException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        setErrorResponse(response, TokenErrorCode.EXPIRED_TOKEN);
+    }
+
+    private void setErrorResponse(HttpServletResponse response, ErrorCode errorCode)
+        throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        ErrorDTO errorDTO = ErrorDTO.from(errorCode);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String errorJson = objectMapper.writeValueAsString(errorDTO);
+
+        if (response.getWriter() != null) {
+            response.getWriter().write(errorJson);
+        }
     }
 }
