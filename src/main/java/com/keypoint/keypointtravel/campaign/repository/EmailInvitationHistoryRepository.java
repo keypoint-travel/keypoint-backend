f8915700204b81228208f8b16b74a76e7b9b8aeb
Khdscor <khds_cor@naver.com>
2024-09-24 15:29:01 +0900

    feature : 캠페인 종료 시 redis 내 캠페인 아이디에 해당하는 모든 항목 제거

--- src/main/java/com/keypoint/keypointtravel/campaign/repository/EmailInvitationHistoryRepository.java
+++ src/main/java/com/keypoint/keypointtravel/campaign/repository/EmailInvitationHistoryRepository.java
@@ -1,10 +1,13 @@
 package com.keypoint.keypointtravel.campaign.repository;
 
 import com.keypoint.keypointtravel.campaign.entity.EmailInvitationHistory;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmailInvitationHistoryRepository extends CrudRepository<EmailInvitationHistory, String> {
 
     Optional<EmailInvitationHistory> findByCampaignIdAndEmail(Long campaignId, String email);
+
+    List<EmailInvitationHistory> findAllByCampaignId(Long campaignId);
 }
--- src/main/java/com/keypoint/keypointtravel/campaign/repository/InvitationProhibitionHistoryRepository.java
+++ src/main/java/com/keypoint/keypointtravel/campaign/repository/InvitationProhibitionHistoryRepository.java
@@ -1,10 +1,13 @@
 package com.keypoint.keypointtravel.campaign.repository;
 
 import com.keypoint.keypointtravel.campaign.entity.InvitationProhibitionHistory;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface InvitationProhibitionHistoryRepository extends
     CrudRepository<InvitationProhibitionHistory, String> {
 
     boolean existsByCampaignId(Long campaignId);
+
+    List<InvitationProhibitionHistory> findAllByCampaignId(Long campaignId);
 }
