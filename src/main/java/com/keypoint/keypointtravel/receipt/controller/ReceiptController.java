6daa1526edcfd32ab1f104b4e2b19c14fbb70498
chea-young <lchy0413@gmail.com>
2024-09-25 14:57:11 +0900

    [Feature] Change API method

--- src/main/java/com/keypoint/keypointtravel/receipt/controller/ReceiptController.java
+++ src/main/java/com/keypoint/keypointtravel/receipt/controller/ReceiptController.java
@@ -16,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +34,7 @@ public class ReceiptController {
     private final MobileReceiptService mobileReceiptService;
 
     @PreAuthorize("hasRole('ROLE_CERTIFIED_USER')")
-    @GetMapping("/analyze")
+    @PostMapping("/analyze")
     public APIResponseEntity<ReceiptOCRResponse> analyzeReceipt(
         @RequestPart(value = "receiptImage", required = false) MultipartFile receiptImage
     ) {
