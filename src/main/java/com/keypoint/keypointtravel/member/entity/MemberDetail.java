4979e00d184ab46b89422b97036007631e81ec30
chea-young <lchy0413@gmail.com>
2024-09-28 13:27:19 +0900

    [Feature] Change relation (#197)

--- src/main/java/com/keypoint/keypointtravel/member/entity/MemberDetail.java
+++ src/main/java/com/keypoint/keypointtravel/member/entity/MemberDetail.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
@@ -36,7 +37,7 @@ public class MemberDetail extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "representative_badge_id")
     private Badge representativeBadge;
 
