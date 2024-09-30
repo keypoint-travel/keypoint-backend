7d568c9e743446319c070ac389a1d4959868df1d
su-jin02 <hera4224@naver.com>
2024-09-30 21:53:22 +0900

    fix : 테마 에러 메세지 오타 수정

--- src/main/java/com/keypoint/keypointtravel/global/enumType/error/ThemeErrorCode.java
+++ src/main/java/com/keypoint/keypointtravel/global/enumType/error/ThemeErrorCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ThemeErrorCode implements ErrorCode {
     NOT_EXISTED_Theme("001_NOT_EXISTED_Theme", "존재하지 않는 테마 입니다."),
-    NOT_EXISTED_Member_Theme("002_NOT_EXISTED_Member_Theme", "해당 유저이 테마가 지정되어있지 않습니다..");
+    NOT_EXISTED_Member_Theme("002_NOT_EXISTED_Member_Theme", "해당 유저의 테마가 지정되어있지 않습니다.");
 
     private final String code;
     private final String msg;
