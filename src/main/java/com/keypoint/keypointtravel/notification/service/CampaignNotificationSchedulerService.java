dcb0d1e7be80a29c0f654a9174348dd1e412a5e2
chea-young <lchy0413@gmail.com>
2024-10-09 20:03:05 +0900

    [Feature] Add schedule about CAMPAIGN_D_DAY (#111) - 캠페인 시작 1일 전 알림 전달 스케줄 추가

--- src/main/java/com/keypoint/keypointtravel/notification/service/CampaignNotificationSchedulerService.java
+++ src/main/java/com/keypoint/keypointtravel/notification/service/CampaignNotificationSchedulerService.java
@@ -6,6 +6,7 @@ import com.keypoint.keypointtravel.global.enumType.notification.PushNotification
 import com.keypoint.keypointtravel.global.utils.DateUtils;
 import com.keypoint.keypointtravel.global.utils.LogUtils;
 import com.keypoint.keypointtravel.notification.event.pushNotification.CommonPushNotificationEvent;
+import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -22,25 +23,61 @@ public class CampaignNotificationSchedulerService {
     private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
-    @Scheduled(cron = "0 46 * * * *", zone = "Asia/Seoul")
-    //@Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     public void sendCampaignDMinus7Notification() {
-        // 캠페인
-        LocalDate compareDate = LocalDate.now().minusDays(7);
-        List<AlarmCampaignUseCase> useCases = campaignRepository.findAlarmCampaignByStartAt(
+        try {
+            LocalDate compareDate = LocalDate.now().minusDays(7);
+            int result = sendCampaignNotificationOnStartAt(
+                PushNotificationType.CAMPAIGN_D_MINUS_7,
+                DateUtils.convertLocalDateToDate(compareDate)
+            );
+
+            LogUtils.writeInfoLog("sendCampaignDMinus7Notification",
+                "Send notifications 7 days before campaign starts " + result);
+        } catch (Exception ex) {
+            LogUtils.writeErrorLog("sendCampaignDMinus7Notification",
+                "Fail to send notifications 7 days before campaign starts", ex);
+        }
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 02 * * * *", zone = "Asia/Seoul")
+    //@Scheduled(cron = "0 0 0 * * *")
+    public void sendCampaignDMinus1Notification() {
+        LocalDate compareDate = LocalDate.now().minusDays(1);
+        int result = sendCampaignNotificationOnStartAt(
+            PushNotificationType.CAMPAIGN_D_DAY,
             DateUtils.convertLocalDateToDate(compareDate)
+        );
+
+        LogUtils.writeInfoLog("sendCampaignDMinus1Notification",
+            "Send notifications 1 days before campaign starts " + result);
+    }
+
+    /**
+     * 특정 날짜가 캠페인 시작날짜인 캠페인 참여자에게 알림을 전송하는 함수
+     *
+     * @param pushNotificationType 알림 타입
+     * @param startAt              캠페인 시작 날짜
+     * @return 알림이 전송된 캠페인의 수
+     */
+    private int sendCampaignNotificationOnStartAt(
+        PushNotificationType pushNotificationType,
+        Date startAt
+    ) {
+        List<AlarmCampaignUseCase> useCases = campaignRepository.findAlarmCampaignByStartAt(
+            startAt
         );
 
         for (AlarmCampaignUseCase useCase : useCases) {
             eventPublisher.publishEvent(
                 CommonPushNotificationEvent.of(
-                    PushNotificationType.CAMPAIGN_D_MINUS_7,
+                    pushNotificationType,
                     useCase.getMemberIds()
                 )
             );
         }
 
-        LogUtils.writeInfoLog("sendCampaignDMinus7Notification",
-            "Send notifications 7 days before campaign starts " + useCases.size());
+        return useCases.size();
     }
 }
