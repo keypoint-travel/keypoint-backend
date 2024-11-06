99280b064359968c0208afebb729fe26c5799b46
chea-young <lchy0413@gmail.com>
2024-11-06 23:25:27 +0900

    [Feature] Add controller about member statistic (#225)

---
+++ src/main/java/com/keypoint/keypointtravel/member/dto/response/StatisticResponse.java
@@ -0,0 +1,12 @@
+package com.keypoint.keypointtravel.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StatisticResponse {
+
+    private String date;
+    private long value;
+}
