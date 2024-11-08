1456e3e3ee0fa075b062ddd9c3a752535e3e0f4a
khdscor <khds_cor@naver.com>
2024-11-08 17:07:53 +0900

    feature: 문의 관련 예외 문구 언어별 추가

--- src/main/java/com/keypoint/keypointtravel/global/enumType/error/InquiryErrorCode.java
+++ src/main/java/com/keypoint/keypointtravel/global/enumType/error/InquiryErrorCode.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum InquiryErrorCode implements ErrorCode {
-    TOO_MANY_IMAGES("001_TOO_MANY_IMAGES", "이미지는 최대 5개까지 등록 가능합니다.");
+    TOO_MANY_IMAGES("001_TOO_MANY_IMAGES", "이미지는 최대 5개까지 등록 가능합니다."),
+    NOT_EXISTED_INQUIRY("002_NOT_EXISTED_INQUIRY", "해당 문의 사항을 찾을 수 없습니다."),
+    DELETED_OR_REPLIED_INQUIRY("003_DELETED_OR_REPLIED_INQUIRY", "삭제되었거나 답변이 완료된 문의 사항입니다.");
 
     private final String code;
     private final String msg;
