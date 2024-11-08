349dc8e9c07b8bab412445b65f2150ef057bb496
khdscor <khds_cor@naver.com>
2024-11-08 16:16:56 +0900

    feature : 문의하기 repository 구현

---
+++ src/main/java/com/keypoint/keypointtravel/inquiry/repository/InquiryDetailImageRepository.java
@@ -0,0 +1,7 @@
+package com.keypoint.keypointtravel.inquiry.repository;
+
+import com.keypoint.keypointtravel.inquiry.entity.InquiryDetailImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InquiryDetailImageRepository extends JpaRepository<InquiryDetailImage, Long> {
+}
